06e9345335cbdadb892b84e521d8497c112bcfed
Andreas Gal <andreas.gal@gmail.com>
2014-07-16 23:08:47 -0700

    more tests

--- tests/TestOps.java
+++ tests/TestOps.java
@@ -13,6 +13,14 @@ public class TestOps {
 	return a / b;
     }
 
+    static int irem(int a, int b) {
+	return a % b;
+    }
+
+    static int imul(int a, int b) {
+	return a * b;
+    }
+
     static float fadd(float a, float b) {
 	return a + b;
     }
@@ -37,6 +45,9 @@ public class TestOps {
 	Assert("Do asserts work", true);
 	Assert("add two ints", iadd(1, 2) == 3);
 	Assert("int div", idiv(8, 2) == 4);
+	Assert("int rem", irem(10, 3) == 1);
+	Assert("int mul", imul(20, 3) == 60);
+	Assert("int mul overflow", imul(0x12345678, 0x12345678) == 502585408);
 	Assert("overflow", iadd(0x7fffffff, 1) == -2147483648);
 	Assert("underflow", iadd(-2147483648, -1) == 0x7fffffff);
 	Assert("add two floats", fadd(1.0f, 2.0f) == 3.0f);
