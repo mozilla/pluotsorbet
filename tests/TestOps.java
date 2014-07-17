78aa40e8aaf7c5da7c9d16121b9c31e231e8d237
Andreas Gal <andreas.gal@gmail.com>
2014-07-16 21:02:27 -0700

    more float tests

--- tests/TestOps.java
+++ tests/TestOps.java
@@ -13,12 +13,18 @@ public class TestOps {
 	return a + b;
     }
 
+    static float fneg(float a) {
+	return -a;
+    }
+
     public static void main(String[] args) {
 	Assert("Do asserts work", true);
 	Assert("add two ints", iadd(1, 2) == 3);
 	Assert("overflow", iadd(0x7fffffff, 1) == -2147483648);
 	Assert("underflow", iadd(-2147483648, -1) == 0x7fffffff);
 	Assert("add two floats", fadd(1.0f, 2.0f) == 3.0f);
+	Assert("max float + 1", fadd(Float.MAX_VALUE, 1) == Float.MAX_VALUE);
+	Assert("float neg", fneg(1.0f) == -1.0f);
     }
 }
 
