e050fc1d59ee69f0be0289887eae937a55936d41
Andreas Gal <andreas.gal@gmail.com>
2014-07-17 22:10:11 -0700

    make test case harder

--- tests/TestException.java
+++ tests/TestException.java
@@ -1,7 +1,14 @@
 package tests;
 
 public class TestException {
+    static void throw1() {
+	try {
+	    throw new RuntimeException("Hello, this is error 1");
+	} catch (Exception e) {
+	}
+    }
+
     public static void main(String[] args) {
-	throw new RuntimeException("Hello, this is Error");
+	throw1();
     }
 }
