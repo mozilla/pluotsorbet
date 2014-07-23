72564b5f4be0cda94b37bbdbab305975732e29ef
Andreas Gal <andreas.gal@gmail.com>
2014-07-23 14:00:36 -0700

    add context.js

---
+++ context.js
@@ -0,0 +1,9 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+function Context(thread) {
+  this.thread = thread;
+  this.level = 0;
+}
