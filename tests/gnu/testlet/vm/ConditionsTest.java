d7672128576a0595e77649a405dcd374f1da1d9d
Andreas Gal <andreas.gal@gmail.com>
2014-07-28 17:15:17 -0700

    move conditions test

---
+++ tests/gnu/testlet/vm/ConditionsTest.java
@@ -0,0 +1,35 @@
+package gnu.testlet.vm;
+
+import gnu.testlet.*;
+
+public class ConditionsTest implements Testlet {
+	public void test(TestHarness th) {
+		String a, b, c;
+		a = "smt";
+		b = a;
+		c = "smt2";
+		int d = 9;
+		int e = -9;
+		int f = 0;
+		String g = null;
+		th.check(a == b);
+		th.check(a != c);
+		th.check(a != null);
+		th.check(g == null);
+		th.check(d == 9);
+		th.check(d > 7);
+		th.check(d >= 9);
+		th.check(d >= 7);
+		th.check(7 < d);
+		th.check(9 <= d);
+		th.check(7 <= d);
+		th.check(d != 6);
+		th.check(d >= 0);
+		th.check(e <= 0);
+		th.check(f >= 0);
+		th.check(f <= 0);
+		th.check(f == 0);
+		th.check(e != 0);
+		th.check(d != 0);
+	}
+}
