3747b184594e0395e0c6425d731c2fd283fa4b66
Andreas Gal <andreas.gal@gmail.com>
2014-07-29 09:40:33 -0700

    move another test

---
+++ tests/gnu/testlet/vm/LongTest.java
@@ -0,0 +1,19 @@
+package gnu.testlet.vm;
+
+import gnu.testlet.*;
+
+public class LongTest implements Testlet {
+    static long h = 0x0123456789abcdefL;
+    static long a = 5;
+    static long b = 7;
+    static long c = 50;
+
+    public void test(TestHarness th) {
+        th.check(("" + h), "81985529216486895");
+	th.check(a + b, 12L);
+	th.check(a - b, -2L);
+	th.check(a * b, 35L);
+	th.check(c / a, 10L);
+	th.check(c % b, 1L);
+    }
+}
