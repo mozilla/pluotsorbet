7ea0d3bd78d77965c761f7a8a1108482f5d51b2d
Andreas Gal <andreas.gal@gmail.com>
2014-07-29 13:43:18 -0700

    more date test

---
+++ tests/gnu/testlet/java/lang/Date/DateTest.java
@@ -0,0 +1,19 @@
+package gnu.testlet.java.lang.Date;
+
+import java.util.Date;
+import gnu.testlet.*;
+
+public class DateTest implements Testlet {
+    public void test(TestHarness th) {
+		Date d = new Date();
+		// This test is not reliable:
+		// compare(d.getTime(), System.currentTimeMillis());
+		Date d2 = new Date(4873984739798L);
+		th.check(d2.getTime(), 4873984739798L);
+		th.check(!d.equals(d2));
+		th.check(d2.hashCode(), -803140168);
+		th.check(d2.toString().indexOf("2124") != -1);
+		d.setTime(4873984739798L);
+		th.check(d.equals(d2));
+	}
+}
