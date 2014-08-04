ae98ebd16edf0d8e76859469679a54bcdb2640eb
Donovan Preston <donovanpreston@gmail.com>
2014-08-04 19:21:26 -0400

    Animate the simple hello graphics application.

--- tests/HelloMid.java
+++ tests/HelloMid.java
@@ -4,13 +4,36 @@ import javax.microedition.lcdui.*;
 import javax.microedition.midlet.*;
 
 
-class HelloGraphics extends Canvas {
+class HelloGraphics extends Canvas implements Runnable {
+    int position = 0;
+    boolean increment = true;
+
     protected void paint(Graphics g) {
         g.setColor(0x00FFFFFF);;
         g.fillRect(0, 0, getWidth() - 1, getHeight() - 1);
         g.setColor(0x00000000);
-        for (int i = 0; i < getWidth() / 5; i++) {
-            g.drawLine(0 + (i * 5), 0, getWidth() - (i * 5), getHeight());
+        g.drawLine(0 + position, 0, getWidth() - position, getHeight());
+    }
+
+    public void run() {
+        while (true) {
+            if (increment) {
+                position = position + 5;
+                if (position >= (getWidth() - 5))
+                    increment = false;
+            } else {
+                position = position - 5;
+                if (position <= 5) {
+                    increment = true;
+                }
+            }
+            // Force a screen refresh.
+            repaint();
+            serviceRepaints();
+            try {
+                Thread.currentThread().sleep(20);
+            } catch(java.lang.InterruptedException e) {
+            }
         }
     }
 }
@@ -32,6 +55,9 @@ public class HelloMid extends MIDlet implements CommandListener {
         graf.setCommandListener(this);
 
         display.setCurrent(graf);
+
+        Thread thread = new Thread(graf);
+        thread.start();
     }
 
     public void pauseApp() {
