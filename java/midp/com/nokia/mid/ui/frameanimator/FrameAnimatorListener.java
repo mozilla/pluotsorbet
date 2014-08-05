0b3e45abc799838d1557a424dbca9273c5b90fbe
Myk Melez <myk@mozilla.org>
2014-08-05 14:52:27 -0700

    populate FrameAnimator* stubs

--- java/midp/com/nokia/mid/ui/frameanimator/FrameAnimatorListener.java
+++ java/midp/com/nokia/mid/ui/frameanimator/FrameAnimatorListener.java
@@ -2,4 +2,6 @@ package com.nokia.mid.ui.frameanimator;
 
 public interface FrameAnimatorListener {
 
+    void animate(FrameAnimator animator, int x, int y, short delta, short deltaX, short deltaY, boolean lastFrame);
+
 }
