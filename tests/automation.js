e7b104783b84101dddadac57a46fbc50c408e3dc
Andreas Gal <andreas.gal@gmail.com>
2014-08-04 19:59:13 -0700

    fix error in travis config

--- tests/automation.js
+++ tests/automation.js
@@ -1,6 +1,10 @@
 /* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* vim: set shiftwidth=4 tabstop=4 autoindent cindent expandtab: */
 
+casper.on('remote.message', function(message) {
+    this.echo(message);
+});
+
 casper.test.begin("unit tests", 1, function(test) {
     casper
     .start("http://localhost:8000/index.html?main=RunTests")
