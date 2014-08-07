c3921e99f6a7db78dc051931437ea39d3ab07496
Andreas Gal <andreas.gal@gmail.com>
2014-08-07 16:15:00 -0700

    add test for gfx/CanvasTest

--- tests/automation.js
+++ tests/automation.js
@@ -5,7 +5,7 @@ casper.on('remote.message', function(message) {
     this.echo(message);
 });
 
-casper.test.begin("unit tests", 2, function(test) {
+casper.test.begin("unit tests", 3, function(test) {
     casper
     .start("http://localhost:8000/index.html?main=RunTests")
     .waitForText("DONE", function then() {
@@ -16,6 +16,12 @@ casper.test.begin("unit tests", 2, function(test) {
     .thenOpen("http://localhost:8000/index.html?main=tests/isolate/TestIsolate")
     .waitForText("DONE", function then() {
         test.assertTextExists("m\na ma\n\ma\nma\nma\n1 m1\n2 m2\nma\nr mar\nmar\nc marc\nmarc\n");
+    });
+
+    casper
+    .thenOpen("http://localhost:8000/index.html?main=com/sun/midp/main/MIDletSuiteLoader&midletClassName=gfx/CanvasTest")
+    .waitForText("PAINTED", function then() {
+        test.assert(true);
     });
 
     casper
