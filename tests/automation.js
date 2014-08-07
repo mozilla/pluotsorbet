b7279947944d65866094235bd3c9a0506f3f457a
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-08-07 09:38:30 -0700

    Add a more complicated test for Isolate

--- tests/automation.js
+++ tests/automation.js
@@ -5,12 +5,33 @@ casper.on('remote.message', function(message) {
     this.echo(message);
 });
 
-casper.test.begin("unit tests", 1, function(test) {
+casper.test.begin("unit tests", 2, function(test) {
     casper
     .start("http://localhost:8000/index.html?main=RunTests")
     .waitForText("DONE", function then() {
         test.assertTextExists("DONE: 711 pass, 1 fail", "run unit tests");
-    })
+    });
+
+    casper
+    .thenOpen("http://localhost:8000/index.html?main=tests/isolate/TestIsolate")
+    .waitForText("DONE", function then() {
+    	test.assertTextExists("\
+m\n\
+a ma\n\
+ma\n\
+1 m1\n\
+ma\n\
+2 m2\n\
+ma\n\
+ma\n\
+r mar\n\
+mar\n\
+c marc\n\
+marc\n\
+");
+    });
+
+    casper
     .run(function() {
         test.done();
     });
