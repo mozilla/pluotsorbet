92298212cd1dc6aac377a5b4c07a76f1095c3f9c
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-08-27 16:50:40 -0700

    Test using FileConnection

--- tests/com/sun/midp/publickeystore/TestInputOutputStorage.java
+++ tests/com/sun/midp/publickeystore/TestInputOutputStorage.java
@@ -2,6 +2,11 @@ package com.sun.midp.publickeystore;
 
 import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
+import javax.microedition.io.*;
+import javax.microedition.io.file.*;
+import java.io.*;
 
 public class TestInputOutputStorage implements Testlet {
     public void test(TestHarness th) {
@@ -13,22 +18,22 @@ public class TestInputOutputStorage implements Testlet {
             OutputStream out = file.openOutputStream();
 
             OutputStorage outStorage = new OutputStorage(out);
-            outStorage.writeValue(1, "Marco");
-            outStorage.writeValue(2, new byte[] { 1, 2 });
-            outStorage.writeValue(3, 9999999999999);
-            outStorage.writeValue(4, false);
-            outStorage.writeValue(5, true);
+            outStorage.writeValue((byte)1, "Marco");
+            outStorage.writeValue((byte)2, new byte[] { 1, 2 });
+            outStorage.writeValue((byte)3, 777777777);
+            outStorage.writeValue((byte)4, false);
+            outStorage.writeValue((byte)5, true);
 
             out.close();
 
             InputStream in = file.openInputStream();
 
             InputStorage inStorage = new InputStorage(in);
-            th.check(inStorage.readValue(1), "Marco");
-            th.check(inStorage.readValue(2), new byte[] { 1, 2 });
-            th.check(inStorage.readValue(3), 9999999999999);
-            th.check(inStorage.readValue(4), false);
-            th.check(inStorage.readValue(5));
+            th.check(inStorage.readValue(new byte[] { 1 }), "Marco");
+            th.check(inStorage.readValue(new byte[] { 2 }), new byte[] { 1, 2 });
+            th.check(inStorage.readValue(new byte[] { 3 }), new Long(777777777));
+            th.check(inStorage.readValue(new byte[] { 4 }), new Boolean(false));
+            th.check(inStorage.readValue(new byte[] { 5 }), new Boolean(true));
 
             in.close();
 
