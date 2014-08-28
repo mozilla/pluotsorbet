91d78fa0cec12067c5e1095f0fe7ed2f3e1fd7a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-08-27 21:05:17 -0700

    Make TestWebPublicKeyStore a MIDlet, otherwise it's difficult to set up a security context

--- tests/com/sun/midp/publickeystore/TestWebPublicKeyStore.java
+++ tests/com/sun/midp/publickeystore/TestWebPublicKeyStore.java
@@ -1,17 +1,28 @@
 package com.sun.midp.publickeystore;
 
-import gnu.testlet.TestHarness;
-import gnu.testlet.Testlet;
+import java.util.*;
+import javax.microedition.midlet.*;
 import com.sun.midp.pki.*;
 import com.sun.midp.security.*;
 import com.sun.midp.main.Configuration;
 
-public class TestWebPublicKeyStore implements Testlet {
-    static private class SecurityTrusted implements ImplicitlyTrustedClass {}
+public class TestWebPublicKeyStore extends MIDlet {
+    public TestWebPublicKeyStore() {
+    }
 
-    public void test(TestHarness th) {
-        SecurityToken internalSecurityToken = SecurityInitializer.requestToken(new SecurityTrusted());
-        WebPublicKeyStore.initKeystoreLocation(internalSecurityToken, Configuration.getProperty("com.sun.midp.publickeystore.WebPublicKeyStore"));
-        CertStore cs = WebPublicKeyStore.getTrustedKeyStore();
+    public void startApp() {
+        WebPublicKeyStore cs = WebPublicKeyStore.getTrustedKeyStore();
+        System.out.println(cs.numberOfKeys());
+        PublicKeyInfo keyInfo = cs.getKey(0);
+        // do some tests on keyInfo
+        X509Certificate[] certificates = cs.getCertificates(keyInfo.getOwner());
+        System.out.println(certificates.length);
+        Vector keys = cs.getKeys();
+    }
+
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
     }
 }
