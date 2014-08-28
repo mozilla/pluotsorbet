fd4482cbe930f188bc93f057486831925dbea4d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-08-27 18:08:52 -0700

    Add security initialization to TestWebPublicKeyStore

--- tests/com/sun/midp/publickeystore/TestWebPublicKeyStore.java
+++ tests/com/sun/midp/publickeystore/TestWebPublicKeyStore.java
@@ -3,9 +3,15 @@ package com.sun.midp.publickeystore;
 import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
 import com.sun.midp.pki.*;
+import com.sun.midp.security.*;
+import com.sun.midp.main.Configuration;
 
 public class TestWebPublicKeyStore implements Testlet {
+    static private class SecurityTrusted implements ImplicitlyTrustedClass {}
+
     public void test(TestHarness th) {
+        SecurityToken internalSecurityToken = SecurityInitializer.requestToken(new SecurityTrusted());
+        WebPublicKeyStore.initKeystoreLocation(internalSecurityToken, Configuration.getProperty("com.sun.midp.publickeystore.WebPublicKeyStore"));
         CertStore cs = WebPublicKeyStore.getTrustedKeyStore();
     }
 }
