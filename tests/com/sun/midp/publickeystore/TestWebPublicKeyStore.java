bb6c4cb8caea3e4dd6da79833038d0dd371c446a
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-08-28 09:36:25 -0700

    Print all the certificates

--- tests/com/sun/midp/publickeystore/TestWebPublicKeyStore.java
+++ tests/com/sun/midp/publickeystore/TestWebPublicKeyStore.java
@@ -16,15 +16,23 @@ public class TestWebPublicKeyStore extends MIDlet {
         System.out.println(cs.numberOfKeys());
         PublicKeyInfo keyInfo = cs.getKey(0);
         // do some tests on keyInfo
-        X509Certificate[] certificates = cs.getCertificates(keyInfo.getOwner());
-        System.out.println(certificates.length);
-        X509Certificate aCert = certificates[0];
+        X509Certificate[] certificates1 = cs.getCertificates(keyInfo.getOwner());
+        System.out.println(certificates1.length);
+        X509Certificate aCert = certificates1[0];
         try {
           aCert.verify(aCert.getPublicKey());
         } catch (CertificateException e) {
             e.printStackTrace();
         }
+
+
         Vector keys = cs.getKeys();
+        for (int i = 0; i < keys.size(); i++) {
+            PublicKeyInfo key = (PublicKeyInfo)keys.elementAt(i);
+            X509Certificate[] certificates = cs.getCertificates(key.getOwner());
+            for (int j = 0; j < certificates.length; j++)
+                System.out.println(certificates[j].toString());
+        }
     }
 
     public void pauseApp() {
