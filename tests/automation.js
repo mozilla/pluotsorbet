e40b32edf52dbe10b628aca0c500ed72aa4e2b1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-02 14:06:02 -0700

    Add TestSprite from phoneME

--- tests/automation.js
+++ tests/automation.js
@@ -9,7 +9,7 @@ casper.test.begin("unit tests", 8, function(test) {
     casper
     .start("http://localhost:8000/index.html?main=RunTests")
     .waitForText("DONE", function then() {
-        test.assertTextExists("DONE: 1037 pass, 0 fail, 3 known fail, 0 unknown pass", "run unit tests");
+        test.assertTextExists("DONE: 1199 pass, 0 fail, 3 known fail, 0 unknown pass", "run unit tests");
     }, function onTimeout() {
         test.fail();
     }, 10000);
