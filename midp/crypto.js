1f2a8b164bb254a1ded612f08de2f77a89c58a39
Myk Melez <myk@mozilla.org>
2014-09-02 09:42:25 -0700

    unrefactor hashing code now that we use different impls for SHA-1 and MD5

--- midp/crypto.js
+++ midp/crypto.js
@@ -11,28 +11,6 @@ Native["com/sun/midp/crypto/PRand.getRandomBytes.([BI)Z"] = function(ctx, stack)
 
 MIDP.hashers = new Map();
 
-MIDP.createHasherGetter = function createHasherGetter(algo) {
-    return function (data) {
-      var hasher;
-
-      // Use data[0] to determine the state of the hasher: if 1, we've created
-      // the hasher (and are in the process of hashing); otherwise, we haven't.
-      // That isn't what the data array is intended for, but it works well enough
-      // for our purposes, and the non-native code doesn't use it for anything,
-      // so it doesn't expect it to have any particular values.
-
-      if (data[0] == 1) {
-          hasher = MIDP.hashers.get(data);
-      } else {
-          hasher = algo.create();
-          MIDP.hashers.set(data, hasher);
-          data[0] = 1;
-      }
-
-      return hasher;
-    }
-}
-
 MIDP.getSHA1Hasher = function(data) {
     var hasher;
 
@@ -61,7 +39,25 @@ MIDP.getSHA1Hasher = function(data) {
     return hasher;
 };
 
-MIDP.getMD5Hasher = MIDP.createHasherGetter(CryptoJS.algo.MD5);
+MIDP.getMD5Hasher = function(data) {
+    var hasher;
+
+    // Use data[0] to determine the state of the hasher: if 1, we've created
+    // the hasher (and are in the process of hashing); otherwise, we haven't.
+    // That isn't what the data array is intended for, but it works well enough
+    // for our purposes, and the non-native code doesn't use it for anything,
+    // so it doesn't expect it to have any particular values.
+
+    if (data[0] == 1) {
+        hasher = MIDP.hashers.get(data);
+    } else {
+        hasher = CryptoJS.algo.MD5.create();
+        MIDP.hashers.set(data, hasher);
+        data[0] = 1;
+    }
+
+    return hasher;
+};
 
 MIDP.bin2String = function(array) {
   var result = "";
@@ -76,37 +72,6 @@ Native["com/sun/midp/crypto/SHA.nativeUpdate.([BII[I[I[I[I)V"] = function(ctx, s
         inLen = stack.pop(), inOff = stack.pop(), inBuf = stack.pop();
 
     MIDP.getSHA1Hasher(data).update(inBuf.subarray(inOff, inOff + inLen));
-}
-
-MIDP.createNativeFinal = function createNativeFinal(hasherGetter) {
-    return function(ctx, stack) {
-        var data = stack.pop(), count = stack.pop(), num = stack.pop(), state = stack.pop(),
-            outOff = stack.pop(), outBuf = stack.pop(), inLen = stack.pop(), inOff = stack.pop(),
-            inBuf = stack.pop();
-
-        var hasher = hasherGetter(data);
-
-        if (inBuf) {
-            // digest passes `null` for inBuf, and there are no other callers,
-            // so this should never happen; but I'm including it for completeness
-            // (and in case a subclass ever uses it).
-            hasher.update(MIDP.bin2String(inBuf.subarray(inOff + inLen)));
-        }
-
-        var hash = hasher.finalize();
-
-        for (var i = 0; i < hash.words.length; i++) {
-            outBuf[outOff + i * 4] = (hash.words[i] >> 24) & 0xff;
-            outBuf[outOff + i * 4 + 1] = (hash.words[i] >> 16) & 0xff;
-            outBuf[outOff + i * 4 + 2] = (hash.words[i] >> 8) & 0xff;
-            outBuf[outOff + i * 4 + 3] = hash.words[i] & 0xff;
-        }
-
-        // XXX Call the reset method instead to completely reset the object.
-        data[0] = 0;
-
-        MIDP.hashers.delete(data);
-    }
 }
 
 Native["com/sun/midp/crypto/SHA.nativeFinal.([BII[BI[I[I[I[I)V"] = function(ctx, stack) {
@@ -145,7 +110,34 @@ Native["com/sun/midp/crypto/MD5.nativeUpdate.([BII[I[I[I[I)V"] = function(ctx, s
     MIDP.getMD5Hasher(data).update(MIDP.bin2String(inBuf.subarray(inOff, inOff + inLen)));
 }
 
-Native["com/sun/midp/crypto/MD5.nativeFinal.([BII[BI[I[I[I[I)V"] = MIDP.createNativeFinal(MIDP.getMD5Hasher);
+Native["com/sun/midp/crypto/MD5.nativeFinal.([BII[BI[I[I[I[I)V"] = function(ctx, stack) {
+    var data = stack.pop(), count = stack.pop(), num = stack.pop(), state = stack.pop(),
+        outOff = stack.pop(), outBuf = stack.pop(), inLen = stack.pop(), inOff = stack.pop(),
+        inBuf = stack.pop();
+
+    var hasher = MIDP.getMD5Hasher(data);
+
+    if (inBuf) {
+        // digest passes `null` for inBuf, and there are no other callers,
+        // so this should never happen; but I'm including it for completeness
+        // (and in case a subclass ever uses it).
+        hasher.update(MIDP.bin2String(inBuf.subarray(inOff, inOff + inLen)));
+    }
+
+    var hash = hasher.finalize();
+
+    for (var i = 0; i < hash.words.length; i++) {
+        outBuf[outOff + i * 4] = (hash.words[i] >> 24) & 0xff;
+        outBuf[outOff + i * 4 + 1] = (hash.words[i] >> 16) & 0xff;
+        outBuf[outOff + i * 4 + 2] = (hash.words[i] >> 8) & 0xff;
+        outBuf[outOff + i * 4 + 3] = hash.words[i] & 0xff;
+    }
+
+    // XXX Call the reset method instead to completely reset the object.
+    data[0] = 0;
+
+    MIDP.hashers.delete(data);
+}
 
 Native["com/sun/midp/crypto/RSA.modExp.([B[B[B[B)I"] = function(ctx, stack) {
     var result = stack.pop(), modulus = stack.pop(), exponent = stack.pop(), data = stack.pop();
