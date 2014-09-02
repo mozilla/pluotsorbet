34bc55f5e916f5f516d2bacd78e81f744866f8b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-02 13:59:49 -0700

    Add TestRC4 from phoneME and its dependencies

---
+++ java/midp/java/security/GeneralSecurityException.java
@@ -0,0 +1,57 @@
+/*
+ *   
+ *
+ * Copyright  1990-2009 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions.
+ */
+ 
+package java.security;
+
+/**
+ * The <code>GeneralSecurityException </code> class is a generic 
+ * security exception class that provides type safety for all the 
+ * security-related exception classes that extend from it.
+ * @version 1.4.2
+ */
+
+public class GeneralSecurityException extends Exception {
+
+    /** 
+     * Constructs a GeneralSecurityException with no detail message.  
+     */
+    public GeneralSecurityException() {
+        super();
+    }
+
+    /**
+     * Constructs a GeneralSecurityException with the specified detail
+     * message.
+     * A detail message is a String that describes this particular
+     * exception.
+     *
+     * @param msg the detail message.  
+     */
+    public GeneralSecurityException(String msg) {
+        super(msg);
+    }
+}
+
