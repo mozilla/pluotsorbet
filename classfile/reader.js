0901fb7da584a3a075ba44ba95dbd0b36544c1af
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-03 15:31:57 -0700

    Implement custom decoder for the Java's modified UTF-8

--- classfile/reader.js
+++ classfile/reader.js
@@ -48,10 +48,41 @@ Reader.prototype.readDouble = function() {
     return data;
 }
 
-var Utf8TextDecoder;
+// Decode Java's modified UTF-8 (JVM specs, $ 4.4.7)
+function javaUTF8Decode(buf) {
+  var rv = '';
+
+  var arr = new Uint8Array(buf)
+
+  var i = 0;
+  while (i < arr.length) {
+    var x = arr[i++] & 0xff;
+
+    if (x <= 0x7f) {
+        // Code points in the range '\u0001' to '\u007F' are represented by a
+        // single byte.
+        // The 7 bits of data in the byte give the value of the code point
+        // represented.
+        rv += String.fromCharCode(x);
+    } else if (x <= 0xdf) {
+        // The null code point ('\u0000') and code points in the range '\u0080'
+        // to '\u07FF' are represented by a pair of bytes x and y.
+        var y = arr[i++];
+        rv += String.fromCharCode(((x & 0x1f) << 6) + (y & 0x3f));
+    } else {
+        // Code points in the range '\u0800' to '\uFFFF' are represented by 3
+        // bytes x, y, and z.
+        var y = arr[i++];
+        var z = arr[i++];
+        rv += String.fromCharCode(((x & 0xf) << 12) + ((y & 0x3f) << 6) + (z & 0x3f));
+    }
+  }
+
+  return rv;
+}
 
 Reader.prototype.readString = function(length) {
-    return util.decodeUtf8(this.readBytes(length));
+    return javaUTF8Decode(this.readBytes(length));
 }
 
 Reader.prototype.readBytes = function(length) {
