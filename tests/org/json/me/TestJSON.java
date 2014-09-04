d2fd7181fb3735315115b6f2a2265e218e54d572
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-04 09:57:28 -0700

    Some tests for the org.json.me library

---
+++ tests/org/json/me/TestJSON.java
@@ -0,0 +1,85 @@
+package org.json.me;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.util.Vector;
+
+public class TestJSON implements Testlet {
+    public void test(TestHarness th) {
+        try {
+            String jsonStr = "{\"team\":\"Bursaspor\",\"manager\":\"Ertuğrul Sağlam\",\"year\":\"2010\"}";
+            JSONObject obj = new JSONObject(jsonStr);
+            th.check(obj.getString("team"), "Bursaspor");
+
+            jsonStr = "[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
+            JSONArray array = new JSONArray(jsonStr);
+            th.check("{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}", array.get(1).toString());
+
+            JSONObject obj2 = (JSONObject)array.get(1);
+            th.check("{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}", obj2.get("1").toString());
+
+            jsonStr = "{}";
+            obj = new JSONObject(jsonStr);
+            th.check("{}", obj.toString());
+
+            jsonStr = "[5,]";
+            array = new JSONArray(jsonStr);
+            th.check("[5]", array.toString());
+
+            jsonStr = "[5,,2]";
+            array = new JSONArray(jsonStr);
+            th.check("[5,null,2]", array.toString());
+
+            jsonStr = "[\"hello\\bworld\\\"abc\\tdef\\\\ghi\\rjkl\\n123\\u4e2d\"]";
+            array = new JSONArray(jsonStr);
+            th.check("hello\bworld\"abc\tdef\\ghi\rjkl\n123中", array.get(0).toString());
+
+            jsonStr = "{\"name\":";
+            try {
+                obj = new JSONObject(jsonStr);
+                th.fail("Exception expected");
+            } catch (JSONException e) {
+                th.check(true, "Exception expected");
+            }
+
+            jsonStr = "{\"name\":}";
+            try {
+                obj = new JSONObject(jsonStr);
+                th.fail("Exception expected");
+            } catch (JSONException e) {
+                th.check(true, "Exception expected");
+            }
+
+
+            jsonStr = "{\"name";
+            try {
+                obj = new JSONObject(jsonStr);
+                th.fail("Exception expected");
+            } catch (JSONException e) {
+                th.check(true, "Exception expected");
+            }
+
+
+            jsonStr = "[[null, 123.45, \"a\\\tb c\"}, true]";
+            try{
+                array = new JSONArray(jsonStr);
+                th.fail("Exception expected");
+            } catch (JSONException e) {
+                th.check(true, "Exception expected");
+            }
+
+            array = new JSONArray();
+            th.check("[]", array.toString());
+
+            Vector testList = new Vector();
+            testList.addElement("First item");
+            testList.addElement("Second item");
+            array = new JSONArray(testList);
+            th.check("[\"First item\",\"Second item\"]", array.toString());
+        } catch (Exception e) {
+            th.fail("Unexpected exception: " + e);
+            e.printStackTrace();
+        }
+    }
+}
