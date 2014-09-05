2b4fd7969726b4447566696c2a459deab44b3bcf
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-04 17:07:09 -0700

    Add test that exercises fromJavaString with a substring

--- tests/gnu/testlet/vm/JNIEncodingTest.java
+++ tests/gnu/testlet/vm/JNIEncodingTest.java
@@ -5,9 +5,11 @@ import gnu.testlet.*;
 import javax.microedition.lcdui.Font;
 
 public class JNIEncodingTest implements Testlet {
-	  public void test(TestHarness th) {
-			  Font font = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_LARGE);
-			  String s = "microedition.encoding";
-				System.out.println("PROP: " + font.stringWidth(s.substring(0, 0)));
-	  }
+    public void test(TestHarness th) {
+        Font font = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_LARGE);
+        String s = "marco";
+        th.check(s.substring(0, 0), "");
+        th.check(font.stringWidth(s.substring(0, 0)), font.stringWidth(""));
+        th.check(font.stringWidth(s.substring(0, 1)), font.stringWidth("m"));
+    }
 }
