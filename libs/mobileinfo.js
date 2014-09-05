5c95489e1ade0253f3c44a2d11c9fabb04632dc9
Myk Melez <myk@mozilla.org>
2014-09-05 16:06:38 -0700

    pad MCC/MNC values more lazily

--- libs/mobileinfo.js
+++ libs/mobileinfo.js
@@ -3,21 +3,18 @@
 
 'use strict';
 
-// Use only the info about the first SIM for the time being
+// Use only the info about the first SIM for the time being.
 var mobileInfo = (function() {
-  var pad = function(num, len) {
-    return "0".repeat(len - num.toString().length) + num;
-  };
-
-  // Initialize the object with the URL params and fallback placeholders.
+  // Initialize the object with the URL params and fallback placeholders
+  // for testing/debugging on a desktop.
   var mobileInfo = {
     network: {
-      mcc: pad(urlParams.network_mcc, 3) || "310", // United States
-      mnc: pad(urlParams.network_mnc, 3) || "001",
+      mcc: urlParams.network_mcc || "310", // United States
+      mnc: urlParams.network_mnc || "001",
     },
     icc: {
-      mcc: pad(urlParams.icc_mcc, 3) || "310", // United States
-      mnc: pad(urlParams.icc_mnc, 3) || "001",
+      mcc: urlParams.icc_mcc || "310", // United States
+      mnc: urlParams.icc_mnc || "001",
       msisdn: urlParams.icc_msisdn || "10005551212",
     },
   };
@@ -32,8 +29,8 @@ var mobileInfo = (function() {
   if (mobileConnections) {
     var networkInfo = mobileConnections[0].voice.network;
 
-    mobileInfo.network.mcc = pad(networkInfo.mcc, 3);
-    mobileInfo.network.mnc = pad(networkInfo.mnc, 3);
+    mobileInfo.network.mcc = networkInfo.mcc.toString();
+    mobileInfo.network.mnc = networkInfo.mnc.toString();
 
     // XXX If we're a certified app, then get the ICC (i.e. SIM) values too
     // from mobileConnections[0].iccInfo.
--- native.js
+++ native.js
@@ -122,11 +122,11 @@ Native["java/lang/System.getProperty0.(Ljava/lang/String;)Ljava/lang/String;"] =
         break;
     case "com.nokia.mid.mnc":
         // The concatenation of the MCC and MNC for the ICC (i.e. SIM card).
-        value = mobileInfo.icc.mcc + mobileInfo.icc.mnc;
+        value = util.pad(mobileInfo.icc.mcc, 3) + util.pad(mobileInfo.icc.mnc, 3);
         break;
     case "com.nokia.mid.networkID":
         // The concatenation of MCC and MNC for the network.
-        value = mobileInfo.network.mcc + mobileInfo.network.mnc;
+        value = util.pad(mobileInfo.network.mcc, 3) + util.pad(mobileInfo.network.mnc, 3);
         break;
     case "com.nokia.mid.ui.customfontsize":
         console.warn("Property 'com.nokia.mid.ui.customfontsize' is a stub");
