8a9c4de4025a27d13f7f0a9503fecc61d3fb2385
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-05 18:26:43 -0700

    Don't throw in functions that are actually called

--- java/custom/com/nokia/mid/s40/codec/DataEncoder.java
+++ java/custom/com/nokia/mid/s40/codec/DataEncoder.java
@@ -6,23 +6,24 @@ public class DataEncoder {
     private String data;
 
     public DataEncoder(String name) throws IOException {
-        throw new RuntimeException("DataEncoder not implemented");
+       System.out.println("DataEncoder() not implemented");
     }
     
     public byte[] getData() throws IOException {
-        throw new RuntimeException("DataEncoder::getData() not implemented");
+        System.out.println("DataEncoder::getData() not implemented");
+        return new byte[] { 0 };
     }
     
     public void putStart(int tag, String name) throws IOException {
-        throw new RuntimeException("DataEncoder::putStart(int,String) not implemented");
+        System.out.println("DataEncoder::putStart(int,String) not implemented");
     }
     
     public void putEnd(int tag, String name) throws IOException {
-        throw new RuntimeException("DataEncoder::putEnd(int,String) not implemented");
+        System.out.println("DataEncoder::putEnd(int,String) not implemented");
     }
     
     public void put(int tag, String name, String value) throws IOException {
-        throw new RuntimeException("DataEncoder::put(int,String,String) not implemented");
+        System.out.println("DataEncoder::put(int,String,String) not implemented");
     }
     
     public void put(int tag, String name, boolean value) throws IOException {
@@ -30,7 +31,7 @@ public class DataEncoder {
     }
     
     public void put(int tag, String name, long value) throws IOException {
-        throw new RuntimeException("DataEncoder::put(int,String,long) not implemented");
+        System.out.println("DataEncoder::put(int,String,long) not implemented");
     }
     
     public void put(int tag, String name, double value) throws IOException {
