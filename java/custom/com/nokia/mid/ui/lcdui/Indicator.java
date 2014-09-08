f90f0f9875ac1ce937bcc73d60732a62c4dc59a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-08 16:04:40 -0700

    Don't throw a RuntimeException in some Indicator and IndicatorManager functions (but just log that the functions aren't implemented)

--- java/custom/com/nokia/mid/ui/lcdui/Indicator.java
+++ java/custom/com/nokia/mid/ui/lcdui/Indicator.java
@@ -4,10 +4,10 @@ import javax.microedition.lcdui.Image;
 
 public class Indicator {
     public Indicator(int aInt, Image aImage) {
-        throw new RuntimeException("Indicator(IL...Image;) not implemented (" + aInt + ", " + aImage + ")");
+        System.out.println("Indicator(IL...Image;) not implemented (" + aInt + ", " + aImage + ")");
     }
 
     public void setActive(boolean active) {
-        throw new RuntimeException("Indicator.setActive(Z)V not implemented (" + active + ")");
+        System.out.println("Indicator.setActive(Z)V not implemented (" + active + ")");
     }
 }
