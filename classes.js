cce935dc89a5a06d1ca2e0a2f53ff2e054fac080
Myk Melez <myk@mozilla.org>
2014-09-09 14:37:47 -0700

    remove logging of class files we load

--- classes.js
+++ classes.js
@@ -65,7 +65,6 @@ Classes.prototype.loadClassBytes = function(bytes) {
 }
 
 Classes.prototype.loadClassFile = function(fileName) {
-    console.log("loading " + fileName + " ...");
     var bytes = this.loadFile(fileName);
     if (!bytes)
         throw new (Classes.ClassNotFoundException)(fileName);
