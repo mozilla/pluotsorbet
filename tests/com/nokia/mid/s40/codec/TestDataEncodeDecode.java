cc2cd4d5ff93e09be1328571b609dca640d617eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-09 17:03:54 -0700

    Fix the test to match the expected behavior

--- tests/com/nokia/mid/s40/codec/TestDataEncodeDecode.java
+++ tests/com/nokia/mid/s40/codec/TestDataEncodeDecode.java
@@ -15,12 +15,12 @@ public class TestDataEncodeDecode implements Testlet {
         byte[] data = encoder.getData();
 
         DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
-        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "event");
         th.check(decoder.getType(), DataType.STRUCT);
-        th.check(decoder.getString(DataType.STRING), "value");
+        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "name");
         th.check(decoder.getType(), DataType.STRING);
+        th.check(decoder.getString(DataType.STRING), "value");
         decoder.getEnd(DataType.STRUCT);
     }
 
@@ -33,12 +33,12 @@ public class TestDataEncodeDecode implements Testlet {
         byte[] data = encoder.getData();
         
         DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
-        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "event");
         th.check(decoder.getType(), DataType.STRUCT);
-        th.check(decoder.getString(DataType.METHOD), "value");
+        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "name");
         th.check(decoder.getType(), DataType.METHOD);
+        th.check(decoder.getString(DataType.METHOD), "value");
         decoder.getEnd(DataType.STRUCT);
     }
 
@@ -52,15 +52,15 @@ public class TestDataEncodeDecode implements Testlet {
         byte[] data = encoder.getData();
         
         DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
-        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "event");
         th.check(decoder.getType(), DataType.STRUCT);
-        th.check(decoder.getInteger(DataType.LONG), 42L);
+        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "l1");
         th.check(decoder.getType(), DataType.LONG);
-        th.check(decoder.getInteger(DataType.ULONG), 7L);
+        th.check(decoder.getInteger(DataType.LONG), 42L);
         th.check(decoder.getName(), "l2");
         th.check(decoder.getType(), DataType.ULONG);
+        th.check(decoder.getInteger(DataType.ULONG), 7L);
         decoder.getEnd(DataType.STRUCT);
     }
 
@@ -76,21 +76,19 @@ public class TestDataEncodeDecode implements Testlet {
         byte[] data = encoder.getData();
         
         DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
-        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "event");
         th.check(decoder.getType(), DataType.STRUCT);
-        decoder.getStart(DataType.ARRAY);
+        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "array");
         th.check(decoder.getType(), DataType.ARRAY);
-        th.check(decoder.getInteger(DataType.LONG), 42L);
+        decoder.getStart(DataType.ARRAY);
         th.check(decoder.getName(), "l1");
         th.check(decoder.getType(), DataType.LONG);
-        th.check(decoder.getInteger(DataType.ULONG), 7L);
+        th.check(decoder.getInteger(DataType.LONG), 42L);
         th.check(decoder.getName(), "l2");
         th.check(decoder.getType(), DataType.ULONG);
+        th.check(decoder.getInteger(DataType.ULONG), 7L);
         decoder.getEnd(DataType.ARRAY);
-        th.check(decoder.getName(), "event");
-        th.check(decoder.getType(), DataType.STRUCT);
         decoder.getEnd(DataType.STRUCT);
     }
 
@@ -103,34 +101,28 @@ public class TestDataEncodeDecode implements Testlet {
         encoder.putEnd(DataType.STRUCT, "struct1");
         encoder.putEnd(DataType.STRUCT, "struct2");
         encoder.putEnd(DataType.STRUCT, "struct3");
-        
+
         byte[] data = encoder.getData();
-        
+
         DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
-        decoder.getStart(DataType.STRUCT);
         th.check(decoder.getName(), "struct1");
         th.check(decoder.getType(), DataType.STRUCT);
-
         decoder.getStart(DataType.STRUCT);
+
         th.check(decoder.getName(), "struct2");
         th.check(decoder.getType(), DataType.STRUCT);
-
         decoder.getStart(DataType.STRUCT);
+
         th.check(decoder.getName(), "struct3");
         th.check(decoder.getType(), DataType.STRUCT);
+        decoder.getStart(DataType.STRUCT);
 
-        th.check(decoder.getInteger(DataType.LONG), 42L);
         th.check(decoder.getName(), "value");
         th.check(decoder.getType(), DataType.LONG);
+        th.check(decoder.getInteger(DataType.LONG), 42L);
 
         decoder.getEnd(DataType.STRUCT);
-        th.check(decoder.getName(), "struct2");
-        th.check(decoder.getType(), DataType.STRUCT);
-
         decoder.getEnd(DataType.STRUCT);
-        th.check(decoder.getName(), "struct1");
-        th.check(decoder.getType(), DataType.STRUCT);
-
         decoder.getEnd(DataType.STRUCT);
     }
 
