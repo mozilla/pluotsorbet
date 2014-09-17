a68cc59187bf199e412de22c887c32cfedf3f8b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-16 18:03:00 -0700

    Add error tests

--- tests/com/nokia/mid/s40/codec/TestDataEncodeDecode.java
+++ tests/com/nokia/mid/s40/codec/TestDataEncodeDecode.java
@@ -120,9 +120,9 @@ public class TestDataEncodeDecode implements Testlet {
         encoder.putStart(DataType.STRUCT, "struct2");
         encoder.putStart(DataType.STRUCT, "struct3");
         encoder.put(DataType.LONG, "value", 42L);
-        encoder.putEnd(DataType.STRUCT, "struct1");
-        encoder.putEnd(DataType.STRUCT, "struct2");
         encoder.putEnd(DataType.STRUCT, "struct3");
+        encoder.putEnd(DataType.STRUCT, "struct2");
+        encoder.putEnd(DataType.STRUCT, "struct1");
 
         byte[] data = encoder.getData();
 
@@ -154,7 +154,7 @@ public class TestDataEncodeDecode implements Testlet {
         encoder.putStart(DataType.LIST, "list");
         encoder.put(DataType.STRING, "str1", "val1");
         encoder.put(DataType.STRING, "str2", "val2");
-        encoder.putEnd(DataType.ARRAY, "list");
+        encoder.putEnd(DataType.LIST, "list");
         encoder.putEnd(DataType.STRUCT, "event");
 
         byte[] data = encoder.getData();
@@ -181,6 +181,142 @@ public class TestDataEncodeDecode implements Testlet {
         decoder.getEnd(DataType.STRUCT);
     }
 
+    void testMissingSimple(TestHarness th) throws IOException {
+        DataEncoder encoder = new DataEncoder("whatever");
+        encoder.putStart(DataType.STRUCT, "event");
+        encoder.putStart(DataType.ARRAY, "arr");
+        encoder.put(DataType.STRING, "str1", "val1");
+        encoder.putEnd(DataType.ARRAY, "arr");
+        encoder.putEnd(DataType.STRUCT, "event");
+
+        byte[] data = encoder.getData();
+
+        DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
+        th.check(decoder.getName(), "event");
+        th.check(decoder.getType(), DataType.STRUCT);
+        decoder.getStart(DataType.STRUCT);
+        th.check(decoder.getName(), "arr");
+        th.check(decoder.getType(), DataType.ARRAY);
+        decoder.getStart(DataType.ARRAY);
+        th.check(decoder.getName(), "str1");
+        th.check(decoder.getType(), DataType.STRING);
+        th.check(decoder.getString(DataType.STRING), "val1");
+        try {
+            decoder.getString(DataType.STRING);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "tag (" + DataType.STRING + ") invalid");
+        }
+        try {
+            decoder.getInteger(DataType.ULONG);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "tag (" + DataType.ULONG + ") invalid");
+        }
+        decoder.getEnd(DataType.ARRAY);
+        decoder.getEnd(DataType.STRUCT);
+    }
+
+    void testMissingStartEnd(TestHarness th) throws IOException {
+        DataEncoder encoder = new DataEncoder("whatever");
+        encoder.putStart(DataType.STRUCT, "event");
+        encoder.put(DataType.STRING, "str1", "val1");
+        encoder.putEnd(DataType.STRUCT, "event");
+
+        byte[] data = encoder.getData();
+
+        DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
+        th.check(decoder.getName(), "event");
+        th.check(decoder.getType(), DataType.STRUCT);
+        decoder.getStart(DataType.STRUCT);
+        try {
+            decoder.getStart(DataType.ARRAY);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "no start found " + DataType.ARRAY);
+        }
+        try {
+            decoder.getStart(DataType.STRUCT);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "no start found " + DataType.STRUCT);
+        }
+        try {
+            decoder.getEnd(DataType.ARRAY);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "no end found " + DataType.ARRAY);
+        }
+        th.check(decoder.getName(), "str1");
+        th.check(decoder.getType(), DataType.STRING);
+        th.check(decoder.getString(DataType.STRING), "val1");
+        decoder.getEnd(DataType.STRUCT);
+        try {
+            decoder.getEnd(DataType.ARRAY);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "no end found " + DataType.ARRAY);
+        }
+    }
+
+    void testEndTooLate(TestHarness th) throws IOException {
+        DataEncoder encoder = new DataEncoder("whatever");
+        encoder.putStart(DataType.STRUCT, "event");
+        encoder.putStart(DataType.ARRAY, "array");
+        encoder.put(DataType.STRING, "str1", "val1");
+        encoder.putEnd(DataType.STRUCT, "event");
+        encoder.putEnd(DataType.ARRAY, "array");
+
+        byte[] data = encoder.getData();
+
+        DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
+        th.check(decoder.getName(), "event");
+        th.check(decoder.getType(), DataType.STRUCT);
+        decoder.getStart(DataType.STRUCT);
+        th.check(decoder.getName(), "array");
+        th.check(decoder.getType(), DataType.ARRAY);
+        decoder.getStart(DataType.ARRAY);
+        try {
+            decoder.getEnd(DataType.ARRAY);
+            th.fail("Exception expected");
+        } catch (IOException e) {
+            th.check(e.getMessage(), "no end found " + DataType.ARRAY);
+        }
+        th.check(decoder.getName(), "str1");
+        th.check(decoder.getType(), DataType.STRING);
+        th.check(decoder.getString(DataType.STRING), "val1");
+        try {
+            decoder.getEnd(DataType.STRUCT);
+        } catch (IOException e) {
+            th.check(e.getMessage(), "no end found " + DataType.STRUCT);
+        }
+    }
+
+    void testSkipOver(TestHarness th) throws IOException {
+        DataEncoder encoder = new DataEncoder("whatever");
+        encoder.putStart(DataType.STRUCT, "event");
+        encoder.putStart(DataType.ARRAY, "array");
+        encoder.put(DataType.STRING, "str1", "val1");
+        encoder.put(DataType.STRING, "str2", "val2");
+        encoder.put(DataType.STRING, "str3", "val3");
+        encoder.putEnd(DataType.ARRAY, "array");
+        encoder.put(DataType.STRING, "str4", "val4");
+        encoder.putEnd(DataType.STRUCT, "event");
+
+        byte[] data = encoder.getData();
+
+        DataDecoder decoder = new DataDecoder("whatever", data, 0, data.length);
+        th.check(decoder.getName(), "event");
+        th.check(decoder.getType(), DataType.STRUCT);
+        decoder.getStart(DataType.STRUCT);
+        decoder.getStart(DataType.ARRAY);
+        decoder.getEnd(DataType.ARRAY);
+        th.check(decoder.getName(), "str4");
+        th.check(decoder.getType(), DataType.STRING);
+        th.check(decoder.getString(DataType.STRING), "val4");
+        decoder.getEnd(DataType.STRUCT);
+    }
+
     public void test(TestHarness th) {
         try {
             testString(th);
@@ -190,6 +326,10 @@ public class TestDataEncodeDecode implements Testlet {
             testArray(th);
             testNested(th);
             testList(th);
+            testMissingSimple(th);
+            testMissingStartEnd(th);
+            testEndTooLate(th);
+            testSkipOver(th);
         } catch (Exception e) {
             th.fail("Unexpected exception: " + e);
             e.printStackTrace();
