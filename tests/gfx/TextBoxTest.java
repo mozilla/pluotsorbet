11be705cd62b99da4b74f397117312756ca92bd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-17 10:35:05 -0700

    Add TextBox test

---
+++ tests/gfx/TextBoxTest.java
@@ -0,0 +1,37 @@
+package gfx;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+public class TextBoxTest extends MIDlet implements CommandListener {
+    private Command quitCommand;
+    private Display display;
+    private TextBox textBox;
+
+    public TextBoxTest() {
+        display = Display.getDisplay(this);
+    }
+
+    public void startApp() {
+        quitCommand = new Command("Quit", Command.SCREEN, 1);
+        textBox = new TextBox("Hello World", "Some text", 40, 0);
+        textBox.addCommand(quitCommand);
+        textBox.setCommandListener(this);
+        display.setCurrent(textBox);
+        System.out.println("PAINTED");
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+
+    public void commandAction(Command choice, Displayable displayable) {
+        if (choice == quitCommand) {
+            destroyApp(false);
+            notifyDestroyed();
+        }
+    }
+}
+
