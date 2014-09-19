6de68b69deaac086f80510acc14517ef19191add
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-09-19 14:41:03 -0700

    Add GetRGBDrawRGBNoAlphaTest as todo

--- tests/automation.js
+++ tests/automation.js
@@ -27,6 +27,7 @@ var gfxTests = [
   { name: "gfx/GetRGBDrawRGBTest", maxDifferent: 0 },
   { name: "gfx/GetRGBDrawRGBWidthHeightTest", maxDifferent: 0 },
   { name: "gfx/GetRGBDrawRGBxyTest", maxDifferent: 0 },
+  { name: "gfx/GetRGBDrawRGBNoAlphaTest", maxDifferent: 0, todo: true },
 ];
 
 casper.test.begin("unit tests", 5 + gfxTests.length, function(test) {
