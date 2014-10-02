908b82e4c696d546b56901d3aa78a620491a1819
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-10-02 11:50:19 -0700

    Make SimpleClass::main do something (for loop with function calls)

--- tests/SimpleClass.java
+++ tests/SimpleClass.java
@@ -1,5 +1,28 @@
+interface TestInterface {
+    public void asd();
+}
+
+class TestClass implements TestInterface {
+    public void asd() {
+        int c = 5;
+        c++;
+    }
+}
+
 class SimpleClass {
+    public void asd() {
+        int c = 5;
+        c++;
+    }
+
     public static void main(String[] args) {
         System.out.println("I'm hungry");
+
+        TestInterface a = new TestClass();
+        SimpleClass b = new SimpleClass();
+
+        for (int i = 0; i < 999999; i++) {
+            a.asd();
+        }
     }
 }
