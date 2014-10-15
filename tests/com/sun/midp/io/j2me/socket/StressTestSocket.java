be17fb52ff87564d249057e0f0ec848bec5d03a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2014-10-15 14:51:06 -0700

    Add test that sends/receives data multiple times from a socket and from multiple sockets. Fixes #452.

---
+++ tests/com/sun/midp/io/j2me/socket/StressTestSocket.java
@@ -0,0 +1,61 @@
+package com.sun.midp.io.j2me.socket;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+import java.io.*;
+import javax.microedition.io.*;
+
+public class StressTestSocket implements Testlet {
+    TestHarness th;
+
+    public void test(TestHarness th) {
+        this.th = th;
+
+        try {
+            testMultipleSendsReceivesOnSameSocket();
+            testMultipleSendsReceivesOnMultipleSockets();
+        } catch (IOException e) {
+            th.todo(false, "Exception unexpected: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    void send(OutputStream os, InputStream is, String string) throws IOException {
+        os.write(string.getBytes());
+
+        byte buf[] = new byte[1024];
+        int i = 0;
+        do {
+            buf[i++] = (byte)is.read();
+        } while (buf[i-1] != -1 && buf[i-1] != '\n' && i < buf.length);
+        
+        String received = new String(buf, 0, i);
+        th.todo(received, string);
+    }
+
+    void testMultipleSendsReceivesOnSameSocket() throws IOException {
+        SocketConnection client = (SocketConnection)Connector.open("socket://localhost:50002");
+        OutputStream os = client.openOutputStream();
+        InputStream is = client.openInputStream();
+
+        for (int i = 0; i < 100; i++) {
+            send(os, is, "Message n." + i + "\n");
+        }
+
+        is.close();
+        os.close();
+        client.close();
+    }
+
+    void testMultipleSendsReceivesOnMultipleSockets() throws IOException {
+        for (int i = 0; i < 100; i++) {
+            SocketConnection client = (SocketConnection)Connector.open("socket://localhost:50002");
+            OutputStream os = client.openOutputStream();
+            InputStream is = client.openInputStream();
+            send(os, is, "Message n." + i + "\n");
+            is.close();
+            os.close();
+            client.close();
+        }
+    }
+}
