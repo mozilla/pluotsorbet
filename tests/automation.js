7cd47bdedeb8b9457d168b4bef3a7460e6f44d74
Myk Melez <myk@mozilla.org>
2014-10-14 23:09:05 -0700

    fix merge horkage

--- tests/automation.js
+++ tests/automation.js
@@ -36,15 +36,10 @@ var gfxTests = [
 casper.test.begin("unit tests", 5 + gfxTests.length, function(test) {
     casper
     .start("http://localhost:8000/index.html")
-<<<<<<< HEAD
     .withFrame(0, function() {
         casper.waitForText("DONE", function() {
-            test.assertTextExists("DONE: 70783 pass, 0 fail, 174 known fail, 0 unknown pass", "run unit tests");
+            test.assertTextExists("DONE: 70795 pass, 0 fail, 174 known fail, 0 unknown pass", "run unit tests");
         });
-=======
-    .waitForText("DONE", function() {
-        test.assertTextExists("DONE: 70788 pass, 0 fail, 174 known fail, 0 unknown pass", "run unit tests");
->>>>>>> master
     });
 
     casper
