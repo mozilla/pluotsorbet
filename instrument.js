5cf19380668264948ac9b5f1525ca022e5058098
Myk Melez <myk@mozilla.org>
2014-10-22 17:04:34 -0700

    comment out SSL stream connection logging

--- instrument.js
+++ instrument.js
@@ -111,45 +111,45 @@ var Instrument = {
   },
 };
 
-Instrument.enter["com/sun/midp/ssl/SSLStreamConnection.<init>.(Ljava/lang/String;ILjava/io/InputStream;Ljava/io/OutputStream;Lcom/sun/midp/pki/CertStore;)V"] = function(caller, callee) {
-  var _this = caller.stack.read(6), port = caller.stack.read(4), host = util.fromJavaString(caller.stack.read(5));
-  _this.logBuffer = "SSLStreamConnection to " + host + ":" + port + ":\n";
-};
+// Instrument.enter["com/sun/midp/ssl/SSLStreamConnection.<init>.(Ljava/lang/String;ILjava/io/InputStream;Ljava/io/OutputStream;Lcom/sun/midp/pki/CertStore;)V"] = function(caller, callee) {
+//   var _this = caller.stack.read(6), port = caller.stack.read(4), host = util.fromJavaString(caller.stack.read(5));
+//   _this.logBuffer = "SSLStreamConnection to " + host + ":" + port + ":\n";
+// };
 
-Instrument.enter["com/sun/midp/ssl/Out.write.(I)V"] = function(caller, callee) {
-  var _this = caller.stack.read(3);
-  var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
-  connection.logBuffer += String.fromCharCode(callee.stack.read(1));
-};
+// Instrument.enter["com/sun/midp/ssl/Out.write.(I)V"] = function(caller, callee) {
+//   var _this = caller.stack.read(3);
+//   var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
+//   connection.logBuffer += String.fromCharCode(callee.stack.read(1));
+// };
 
-Instrument.enter["com/sun/midp/ssl/Out.write.([BII)V"] = function(caller, callee) {
-  var len = caller.stack.read(1), off = caller.stack.read(2), b = caller.stack.read(3), _this = caller.stack.read(4);
-  var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
-  var range = b.subarray(off, off + len);
-  for (var i = 0; i < range.length; i++) {
-    connection.logBuffer += String.fromCharCode(range[i] & 0xff);
-  }
-};
+// Instrument.enter["com/sun/midp/ssl/Out.write.([BII)V"] = function(caller, callee) {
+//   var len = caller.stack.read(1), off = caller.stack.read(2), b = caller.stack.read(3), _this = caller.stack.read(4);
+//   var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
+//   var range = b.subarray(off, off + len);
+//   for (var i = 0; i < range.length; i++) {
+//     connection.logBuffer += String.fromCharCode(range[i] & 0xff);
+//   }
+// };
 
-Instrument.exit["com/sun/midp/ssl/In.read.()I"] = function(caller, callee) {
-  var _this = caller.stack.read(3);
-  var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
-  connection.logBuffer += String.fromCharCode(callee.stack.read(1));
-};
+// Instrument.exit["com/sun/midp/ssl/In.read.()I"] = function(caller, callee) {
+//   var _this = caller.stack.read(3);
+//   var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
+//   connection.logBuffer += String.fromCharCode(callee.stack.read(1));
+// };
 
-Instrument.exit["com/sun/midp/ssl/In.read.([BII)I"] = function(caller, callee) {
-  var len = caller.stack.read(4), off = caller.stack.read(5), b = caller.stack.read(6), _this = caller.stack.read(7);
-  var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
-  var range = b.subarray(off, off + len);
-  for (var i = 0; i < range.length; i++) {
-    connection.logBuffer += String.fromCharCode(range[i] & 0xff);
-  }
-};
+// Instrument.exit["com/sun/midp/ssl/In.read.([BII)I"] = function(caller, callee) {
+//   var len = caller.stack.read(4), off = caller.stack.read(5), b = caller.stack.read(6), _this = caller.stack.read(7);
+//   var connection = _this.class.getField("I.ssc.Lcom/sun/midp/ssl/SSLStreamConnection;").get(_this);
+//   var range = b.subarray(off, off + len);
+//   for (var i = 0; i < range.length; i++) {
+//     connection.logBuffer += String.fromCharCode(range[i] & 0xff);
+//   }
+// };
 
-Instrument.enter["com/sun/midp/ssl/SSLStreamConnection.close.()V"] = function(caller, callee) {
-  var _this = caller.stack.read(1);
-  if ("logBuffer" in _this) {
-    console.log(_this.logBuffer);
-    delete _this.logBuffer;
-  }
-};
+// Instrument.enter["com/sun/midp/ssl/SSLStreamConnection.close.()V"] = function(caller, callee) {
+//   var _this = caller.stack.read(1);
+//   if ("logBuffer" in _this) {
+//     console.log(_this.logBuffer);
+//     delete _this.logBuffer;
+//   }
+// };
