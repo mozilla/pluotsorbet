16a7802d124b925f4a456ec370d913e78596e2a0
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2014-11-03 18:02:12 +0100

    Fix indentation

--- tests/httpServer.py
+++ tests/httpServer.py
@@ -7,12 +7,12 @@ class CustomRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.send_header("Pragma", "no-cache")
-	self.send_header("Expires", "0")
+        self.send_header("Expires", "0")
         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
 
 port = 8000
 if len(sys.argv) > 1:
-  port = int(sys.argv[1])
+    port = int(sys.argv[1])
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', port), CustomRequestHandler)
 httpd.serve_forever()
