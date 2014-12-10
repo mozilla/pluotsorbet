f95956009b4ff5af90e7f4aebcaa25b17415168f
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2014-12-10 18:43:46 +0100

    Add comments explaining how we handle notifications in Indicator and IndicatorManager

--- java/custom/com/nokia/mid/ui/lcdui/Indicator.java
+++ java/custom/com/nokia/mid/ui/lcdui/Indicator.java
@@ -2,6 +2,23 @@ package com.nokia.mid.ui.lcdui;
 
 import javax.microedition.lcdui.Image;
 
+/*
+ * This class is undocumented.
+ *
+ * We're implementing indicators using the Notifications API.
+ * When setActive is called with true, we enable showing notifications.
+ * When setActive is called with false, we dismiss notifications.
+ * This class is associated with the nokia.active-standby localmsg
+ * server, that gives us the text associated with the Indicator.
+ * We show a notification when the nokia.active-standby receives an
+ * "Update" event and the Indicator is active.
+ *
+ * We're storing an icon in the image property, but we're not really
+ * using it (we're using the icon sent to the nokia.active-standby
+ * server).
+ *
+ */
+
 public class Indicator {
     Image image;
 
