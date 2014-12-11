5dbe2e5253fe46f339ada919a90cc2c6c5966610
Brendan Dahl <brendan.dahl@gmail.com>
2014-12-11 11:25:27 -0800

    Ensure frame gets pushed on vm pause.

--- runtime.ts
+++ runtime.ts
@@ -600,8 +600,13 @@ module J2ME {
             ? methodInfo.classInfo.getClassObject($.ctx)
             : frame.getLocal(0);
         }
-
-        $.ctx.monitorEnter(frame.lockObject);
+        try {
+          $.ctx.monitorEnter(frame.lockObject);
+        } catch (e) {
+          // Ensure the frame gets pushed on vm pause.
+          $.ctx.frames.push(frame);
+          throw e;
+        }
       }
       return $.ctx.executeNewFrameSet([frame]);
     };
