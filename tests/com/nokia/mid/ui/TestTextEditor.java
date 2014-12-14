da6365b1defe70616759a17a2121839083c83153
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2014-12-14 19:05:52 +0100

    Add Emoji test

--- tests/com/nokia/mid/ui/TestTextEditor.java
+++ tests/com/nokia/mid/ui/TestTextEditor.java
@@ -10,6 +10,71 @@ import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.TextField;
 
 public class TestTextEditor extends Canvas implements Testlet {
+    public static String checkCodeFormat(String code) {
+        if (code == null) {
+            return code;
+        }
+        if (code.length() == 10) {
+            String c1 = code.substring(0, 5);
+            String c2 = code.substring(5);
+            return c1 + " " + c2;
+        } else if (code.length() == 6) {
+            String c1 = code.substring(0, 2);
+            String c2 = code.substring(2);
+            return c1 + " " + c2;
+        } else {
+            return code;
+        }
+    }
+
+    private final static int LEAD_OFFSET = 0xD800 - (0x10000 >> 10);
+
+    public static String getSurrogatePairs(String inputString) {
+        int character;
+        char low, high;
+        int start = 0, end = 0;
+        if (inputString == null) {
+            return inputString;
+        }
+
+        StringBuffer sb = new StringBuffer(1000);
+
+        // Go through all characters in the input.
+        // Space (0x20) is used as separator
+        do {
+            end = inputString.indexOf(" ", start);
+
+            // Space not found -> last sub-string
+            if (end == -1) {
+                end = inputString.length();
+            }
+
+            try {
+                character = Integer.parseInt(inputString.substring(start, end), 16);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            // Anything below 0xffff is not surrogate pair
+            if (character < 0xffff) {
+                sb.append((char) character);
+            } else {
+                // From http://www.unicode.org/faq/utf_bom.html
+                high = (char) (LEAD_OFFSET + (character >> 10));
+                low = (char) (0xDC00 + (character & 0x3FF));
+
+                sb.append(high);
+                sb.append(low);
+            }
+
+            // skip the space
+            start = (end + 1);
+        } while (end != inputString.length());
+
+        return sb.toString();
+    }
+
     public void testConstraints(TestHarness th, int constraints) {
         TextEditor textEditor = new TextEditor("Hello, world!", 20, 0, 100, 24);
 
@@ -115,11 +180,49 @@ public class TestTextEditor extends Canvas implements Testlet {
         textEditor.setParent(null);
     }
 
+    public void testEmoji(TestHarness th) {
+        TextEditor textEditor = TextEditor.createTextEditor("Hello, world!", 200, TextField.ANY, 150, 50);
+
+        textEditor.setParent(this);
+        textEditor.setMultiline(true);
+        textEditor.setBackgroundColor(0x00FFFFFF);
+        textEditor.setForegroundColor(0xFF000000);
+        textEditor.setVisible(true);
+        textEditor.setFocus(true);
+        textEditor.setPosition(0, 0);
+
+        th.check(textEditor.getCaretPosition(), 13);
+
+        String code = getSurrogatePairs(checkCodeFormat("1f609"));
+        textEditor.insert(code, textEditor.getCaretPosition());
+
+        th.check(textEditor.getCaretPosition(), 14);
+
+        textEditor.insert(code, textEditor.getCaretPosition());
+
+        th.check(textEditor.getCaretPosition(), 15);
+
+        th.check(textEditor.size(), 15);
+
+        textEditor.insert(code, 0);
+        th.check(textEditor.getCaretPosition(), 1);
+        th.check(textEditor.size(), 16);
+        textEditor.insert(code, 1);
+        th.check(textEditor.getCaretPosition(), 2);
+        th.check(textEditor.size(), 17);
+        textEditor.delete(1, 1);
+        th.check(textEditor.size(), 16);
+
+        // Emoji + "Hello, world!" + Emoji + Emoji
+        th.check(textEditor.getContent(), code + "Hello, world!" + code + code);
+    }
+
     private native boolean isTextEditorReallyFocused();
 
     public void test(TestHarness th) {
         testConstraints(th, TextField.ANY);
         testConstraints(th, TextField.PASSWORD);
+        testEmoji(th);
     }
 
     protected void paint(Graphics graphics) {}
