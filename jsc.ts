9fc8c5314d16486269f61b00631f45aa585aebe0
Michael Bebenita <mbebenita@gmail.com>
2014-12-18 17:19:46 -0800

    Remove references to old files.

--- jsc.ts
+++ jsc.ts
@@ -67,9 +67,9 @@ module J2ME {
   }
 
   loadFiles("libs/zipfile.js", "blackBox.js", "build/j2me.js", "classfile/classfile.js",
-    "classfile/reader.js", "classfile/tags.js", "classfile/attributetypes.js",
+    "classfile/tags.js", "classfile/attributetypes.js",
     "libs/encoding.js", "util.js", "frame.js",
-    "classfile/accessflags.js", "instrument.js", "signature.js", "opcodes.js",
+    "instrument.js", "signature.js", "opcodes.js",
     "override.js", "native.js", "string.js", "libs/console.js", "midp/midp.js",
     "libs/long.js", "midp/crypto.js", "libs/forge/md5.js", "libs/forge/util.js");
 
