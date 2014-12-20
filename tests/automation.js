edf60386840bd24c374f524e372ad5c1d56f60f7
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2014-12-20 19:43:34 +0100

    Update number of passing tests

--- tests/automation.js
+++ tests/automation.js
@@ -57,7 +57,7 @@ var gfxTests = [
 ];
 
 var expectedUnitTestResults = [
-  { name: "pass", number: 71305 },
+  { name: "pass", number: 71307 },
   { name: "fail", number: 0 },
   { name: "known fail", number: 180 },
   { name: "unknown pass", number: 0 }
