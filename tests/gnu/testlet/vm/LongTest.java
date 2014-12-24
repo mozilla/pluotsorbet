17766744d1cf340908760ddb8fcb2bb38fceefa6
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2014-12-24 01:25:08 +0100

    Test long comparison

--- tests/gnu/testlet/vm/LongTest.java
+++ tests/gnu/testlet/vm/LongTest.java
@@ -7,13 +7,18 @@ public class LongTest implements Testlet {
     static long a = 5;
     static long b = 7;
     static long c = 50;
+    static long d = 5;
 
     public void test(TestHarness th) {
         th.check(("" + h), "81985529216486895");
-	th.check(a + b, 12L);
-	th.check(a - b, -2L);
-	th.check(a * b, 35L);
-	th.check(c / a, 10L);
-	th.check(c % b, 1L);
+    	th.check(a + b, 12L);
+    	th.check(a - b, -2L);
+    	th.check(a * b, 35L);
+    	th.check(c / a, 10L);
+    	th.check(c % b, 1L);
+
+        th.check(a < b);
+        th.check(b > a);
+        th.check(a == d);
     }
 }
