0b7f6e65bd1e484abce803475a25e947477bd8a5
Myk Melez <myk@mozilla.org>
2014-12-31 16:12:10 -0800

    move image comparison of Alert back to gfx test

---
+++ tests/gfx/AlertTest.java
@@ -0,0 +1,34 @@
+package gfx;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+public class AlertTest extends MIDlet {
+    private Display display;
+    private Alert alert;
+
+    public AlertTest() {
+        display = Display.getDisplay(this);
+    }
+
+    public void startApp() {
+        alert = new Alert("Hello World", "Some text", null, AlertType.INFO);
+        display.setCurrent(alert);
+
+        try {
+            do {
+                Thread.sleep(100);
+            } while (!alert.isShown());
+        } catch (InterruptedException e) {
+            System.out.println("FAIL");
+        }
+
+        System.out.println("PAINTED");
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
