c55faf6b961c27b1e0256ab3786bd5bdfa988c24
Myk Melez <myk@mozilla.org>
2015-01-04 23:54:46 -0800

    paint explicit background color (fixes #815)

--- tests/gfx/ImageRenderingTest.java
+++ tests/gfx/ImageRenderingTest.java
@@ -9,6 +9,9 @@ public class ImageRenderingTest extends MIDlet {
 
     class TestCanvas extends Canvas {
         protected void paint(Graphics g) {
+            g.setColor(0x00FFFFFF);
+            g.fillRect(0, 0, getWidth(), getHeight());
+
             if (image != null) {
                 g.drawImage(image, 0, 0, Graphics.TOP | Graphics.LEFT);
             }
