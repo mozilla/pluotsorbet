39b84f726fc0cc36ddae2329e254f2cbdfa379a6
Tim Abraldes <tim@timabraldes.com>
2015-01-07 13:47:01 -0800

    NOP change to make travis run again

--- tests/automation.js
+++ tests/automation.js
@@ -1,6 +1,6 @@
 /* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* vim: set shiftwidth=4 tabstop=4 autoindent cindent expandtab: */
-
+ 
 var system = require('system');
 var fs = require('fs');
 
