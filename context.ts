de226619e831428bc54b426ffeed8b9eb239ed8f
Brendan Dahl <brendan.dahl@gmail.com>
2015-01-16 11:16:57 -0800

    Remove old profiling time.

--- context.ts
+++ context.ts
@@ -254,8 +254,6 @@ module J2ME {
 
   export class Context {
     private static _nextId: number = 0;
-    static _startTime: number = 0;
-    static _totalTime: number = 0;
     private static _colors = [
       IndentingWriter.PURPLE,
       IndentingWriter.YELLOW,
@@ -432,10 +430,6 @@ module J2ME {
     }
 
     private execute() {
-      if (Context._startTime === 0) {
-        Context._startTime = performance.now();
-      }
-      var start = performance.now();
       Instrument.callResumeHooks(this.current());
       this.setAsCurrentContext();
       do {
@@ -452,8 +446,6 @@ module J2ME {
               break;
           }
           U = VMState.Running;
-          var end = performance.now() - start;
-          Context._totalTime += end;
           this.clearCurrentContext();
           return;
         }
