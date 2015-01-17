383358d99ef2dcfdcba8475c52b71247b264a608
Brendan Dahl <brendan.dahl@gmail.com>
2015-01-16 17:52:33 -0800

    Move APP_BASE_DIR so it's defined before init-fs uses it.

--- main.js
+++ main.js
@@ -7,12 +7,6 @@
 // To launch the MIDP demo: ?main=com/sun/midp/main/MIDletSuiteLoader&midletClassName=HelloCommandMIDlet
 // To launch a JAR file: ?main=com/sun/midp/main/MIDletSuiteLoader&args=app.jar
 
-// The base directory of the app, relative to the current page.  Normally this
-// is the directory from which the page was loaded, but some test pages load
-// from a subdirectory, like tests/fs/, and they set this accordingly such that
-// code loads files, like libs/fs-init.js, can load them from the right place.
-var APP_BASE_DIR = "./";
-
 var jvm = new JVM();
 
 var main = config.main || "com/sun/midp/main/MIDletSuiteLoader";
