da9aab9da67b200a5e0d53dadef3390d63a259c3
Myk Melez <myk@mozilla.org>
2015-01-21 14:57:43 -0800

    move default midlet config to config/default.js

--- config/default.js
+++ config/default.js
@@ -3,11 +3,22 @@
 
 'use strict';
 
-var config = {
-};
-
 // The base directory of the app, relative to the current page.  Normally this
 // is the directory from which the page was loaded, but some test pages load
 // from a subdirectory, like tests/fs/, and they set this accordingly such that
 // code loads files, like libs/fs-init.js, can load them from the right place.
 var APP_BASE_DIR = "./";
+
+var config = {
+};
+
+var MIDlet = {
+  name: "aMIDlet",
+
+  SMSDialogVerificationText: "This app sent you an SMS. Type the message you received here:",
+  SMSDialogTimeout: 300000, // Five minutes
+  SMSDialogTimeoutText: "left",
+  SMSDialogReceiveFilter: function(message) {
+    return message;
+  },
+};
--- config/midlet.js
+++ config/midlet.js
@@ -3,13 +3,3 @@
 
 'use strict';
 
-var MIDlet = {
-  name: "aMIDlet",
-
-  SMSDialogVerificationText: "This app sent you an SMS. Type the message you received here:",
-  SMSDialogTimeout: 300000, // Five minutes
-  SMSDialogTimeoutText: "left",
-  SMSDialogReceiveFilter: function(message) {
-    return message;
-  },
-};
