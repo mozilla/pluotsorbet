60a0f672ae0ba3396e3379d6d61f397fc6cf77b7
Myk Melez <myk@mozilla.org>
2015-01-21 16:26:16 -0800

    re-enable app-specific configuration

--- tools/package.sh
+++ tools/package.sh
@@ -31,6 +31,12 @@ cp -R classfile $PACKAGE_DIR/.
 # copy entire contents of libs dir
 cp -R libs $PACKAGE_DIR/.
 
+# copy entire contents of config dir
+cp -R config $PACKAGE_DIR/.
+
+# Merge app.js into a config file that gets loaded.
+cat $PACKAGE_DIR/config/app.js >> $PACKAGE_DIR/config/midlet.js && rm $PACKAGE_DIR/config/app.js
+
 # copy entire contents of midp dir
 cp -R midp $PACKAGE_DIR/.
 
