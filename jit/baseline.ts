de7b8f8db050491594e104bb8e04cc6eadd752f2
Michael Bebenita <mbebenita@gmail.com>
2015-01-26 12:30:57 -0800

    Fix the way native arrays are created in the baseline compiler.

--- jit/baseline.ts
+++ jit/baseline.ts
@@ -145,8 +145,9 @@ module J2ME {
       case Kind.Short:
         return "Int16Array";
       case Kind.Byte:
-      case Kind.Boolean:
         return "Int8Array";
+      case Kind.Boolean:
+        return "Uint8Array";
       case Kind.Float:
         return "Float32Array";
       case Kind.Long:
