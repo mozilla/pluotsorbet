56f4ec662cd71448ac10a310489d6b06d6a36255
Michael Bebenita <mbebenita@gmail.com>
2015-01-30 23:30:10 -0800

    Update JIT benchmark.

--- tests/JITBenchmark.java
+++ tests/JITBenchmark.java
@@ -1,5 +1,6 @@
 import java.lang.Object;
 import java.lang.System;
+import java.util.Hashtable;
 import java.util.Vector;
 import java.io.ByteArrayOutputStream;
 
@@ -40,22 +41,109 @@ class JITBenchmark {
     System.out.println("Length: " + stream.size());
   }
 
+  public static void concatStrings() {
+    String s = "";
+    int count = size / 16;
+    for (int i = 0; i < count; i++) {
+      s += "X";
+    }
+    System.out.println("Length: " + s.length());
+  }
+
+  public static void getBytes() {
+    String s = "getBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytesgetBytes";
+    int count = size / 4;
+    byte [] bytes = null;
+    for (int i = 0; i < count; i++) {
+      bytes = s.getBytes();
+    }
+    System.out.println("Length: " + bytes.length);
+  }
+
+  public static void synch() {
+    int count = size * 10;
+    byte [] bytes = null;
+    Object o = new Object();
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+      synchronized (o) {
+        sum++;
+      }
+      synchronized (o) {
+        sum++;
+      }
+      synchronized (o) {
+        sum++;
+      }
+      synchronized (o) {
+        sum++;
+      }
+      synchronized (o) {
+        sum++;
+      }
+      synchronized (o) {
+        sum++;
+      }
+    }
+    System.out.println("Sum: " + sum);
+  }
+
+  public static void hashtable() {
+    int count = size * 2;
+    Hashtable hash = new Hashtable();
+    Object o = new Object();
+    String [] names = {
+      "hello", "world", "hello1", "world2", "hello3", "world4", "hello5", "world6"
+    };
+    for (int i = 0; i < count; i++) {
+      String name = names[i % names.length];
+      hash.put(name, o);
+      hash.get(name);
+      hash.put(name, o);
+      hash.get(name);
+      hash.put(name, o);
+      hash.get(name);
+      hash.put(name, o);
+      hash.get(name);
+    }
+  }
+
   public static void main(String[] args) {
     long start = 0;
 
     start = System.currentTimeMillis();
     System.out.println("createObjectArrays: ...");
-    // createObjectArrays();
+    createObjectArrays();
     System.out.println("createObjectArrays: " + (System.currentTimeMillis() - start));
 
     start = System.currentTimeMillis();
     System.out.println("createPrimitiveArrays: ...");
-    // createPrimitiveArrays();
+    createPrimitiveArrays();
     System.out.println("createPrimitiveArrays: " + (System.currentTimeMillis() - start));
 
     start = System.currentTimeMillis();
     System.out.println("writeByteArrayOutputStream: ...");
     writeByteArrayOutputStream();
     System.out.println("writeByteArrayOutputStream: " + (System.currentTimeMillis() - start));
+
+    start = System.currentTimeMillis();
+    System.out.println("concatStrings: ...");
+    concatStrings();
+    System.out.println("concatStrings: " + (System.currentTimeMillis() - start));
+
+    start = System.currentTimeMillis();
+    System.out.println("getBytes: ...");
+    getBytes();
+    System.out.println("getBytes: " + (System.currentTimeMillis() - start));
+
+    start = System.currentTimeMillis();
+    System.out.println("synchronize: ...");
+    synch();
+    System.out.println("synchronize: " + (System.currentTimeMillis() - start));
+
+    start = System.currentTimeMillis();
+    System.out.println("hashtable: ...");
+    hashtable();
+    System.out.println("hashtable: " + (System.currentTimeMillis() - start));
   }
 }
