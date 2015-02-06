68882820e8227d69bd801b723f5a5b30f17bfca6
Myk Melez <myk@mozilla.org>
2015-02-05 17:08:55 -0800

    replace commented-out tests of obsolete private API with tests of current public one

--- tests/com/ibm/oti/connection/file/TestFileConnection.java
+++ tests/com/ibm/oti/connection/file/TestFileConnection.java
@@ -386,65 +386,39 @@ public class TestFileConnection implements Testlet {
             file.delete();
             file.close();
 
-            // Opening an output stream to a nonexistent file should create it.
-            // file = (FileConnection)Connector.open(dirPath + "provaDir/nonexistent.txt");
-            // th.check(!file.exists());
-            // // Create the stream ourselves because Connection.openOutputStream
-            // // raises an exception when the file doesn't exist.
-            // out = new FCOutputStream((file.getPath() + file.getName()).getBytes("UTF-8"), null);
-            // th.check(file.exists());
-            // th.check(file.fileSize(), 0, "Check file size");
-            // out.close();
-            // file.delete();
-            // file.close();
+            // Opening an output stream to a nonexistent file should throw
+            // an exception.
+            file = (FileConnection)Connector.open(dirPath + "provaDir/nonexistent.txt");
+            th.check(!file.exists());
+            try {
+                out = file.openOutputStream();
+                th.fail("Exception expected opening output stream to nonexistent file");
+            } catch (IOException e) {
+                th.check(e.getMessage(), "Target file doesn't exist");
+            }
+            th.check(!file.exists());
+            out.close();
+            file.close();
 
-            // Opening an output stream to a nonexistent file should create it
-            // even if we specify an offset.  FCOutputStream doesn't say what to
-            // do if offset > fileSize, and our impl simply ignores the problem,
-            // although Connection sets offset to fileSize in that case,
-            // and presumably all callers are going to go through Connection
-            // rather than instantiating FCOutputStream directly, so our impl
-            // should be ok.
-            // file = (FileConnection)Connector.open(dirPath + "provaDir/nonexistent.txt");
-            // th.check(!file.exists());
-            // // Create the stream ourselves because Connection.openOutputStream
-            // // raises an exception when the file doesn't exist.
-            // out = new FCOutputStream((file.getPath() + file.getName()).getBytes("UTF-8"), 5, null);
-            // th.check(file.exists());
-            // th.check(file.fileSize(), 0, "Check file size");
-            // out.close();
-            // file.delete();
-            // file.close();
-
-            // Test that deleting or renaming a open file fails.
-            // Create the stream ourselves because otherwise FileConnection will
-            // close the files before deleting/renaming them.
-            // file = (FileConnection)Connector.open(dirPath + "provaDir/nonexistent.txt");
-            // out = new FCOutputStream((file.getPath() + file.getName()).getBytes("UTF-8"), null);
-            // try {
-            //     file.delete();
-            //     th.fail("Exception expected");
-            // } catch (IOException e) {
-            //     th.check(e.getMessage(), "Can not delete: " + file.getURL());
-            // }
-            // try {
-            //     file.rename("newname");
-            //     th.fail("Exception expected");
-            // } catch (IOException e) {
-            //     th.check(e.getMessage(), "Rename failed");
-            // }
-            // out.close();
-
-            // Renaming before closing the file should succeed.
+            // Deleting a file with an open output stream succeeds.
             file = (FileConnection)Connector.open(dirPath + "provaDir/nonexistent.txt");
             file.create();
+            out = file.openOutputStream();
+            file.delete();
+            th.check(!file.exists());
+
+            // Renaming a file with an open output stream succeeds.
+            file.create();
+            out = file.openOutputStream();
             file.rename("newname");
-            file.close();
+            th.check(file.getName(), "newname");
+            file.delete();
 
             // The file with the old name doesn't exist anymore.
             file = (FileConnection)Connector.open(dirPath + "provaDir/nonexistent.txt");
             th.check(!file.exists());
             file.close();
+
             // The file with the new name exists.
             file = (FileConnection)Connector.open(dirPath + "provaDir/newname");
             th.check(file.exists());
