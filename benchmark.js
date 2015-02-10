3196d154b618a90f1b19d4c980190d5079568d6b
Brendan Dahl <brendan.dahl@gmail.com>
2015-02-09 18:53:43 -0800

    Add gui.

--- benchmark.js
+++ benchmark.js
@@ -7,44 +7,67 @@ var Benchmark = (function() {
     return array.reduce(add, 0) / array.length;
   }
 
-  var startup = {
+  var defaultStorage = {
     numRounds: 10,
     roundDelay: 5000, // ms to delay starting next round of tests
-    startTime: null,
     baseline: [],
-    run: function() {
-      this.round = 0;
-      this.times = [];
-      this.running = true;
+    running: false,
+    round: 0,
+    times: [],
+    deleteFs: true,
+    deleteJitCache: true,
+    buildBaseline: false
+  };
+
+  function Storage(key, defaults) {
+    this.key = key;
+    if (!(key in localStorage)) {
+      this.storage = defaults;
+      this.save();
+    }
+    this.storage = JSON.parse(localStorage[key]);
+    Object.keys(defaultStorage).forEach(function(key) {
+      Object.defineProperty(this, key, {
+        get: function() { return this.storage[key]; },
+        set: function(newValue) { this.storage[key] = newValue; this.save() },
+        enumerable: true,
+        configurable: true
+      });
+    }, this);
+  }
+
+  Storage.prototype = {
+    save: function() {
+      localStorage[this.key] = JSON.stringify(this.storage);
+    }
+  };
+
+  var storage = new Storage("benchmark", defaultStorage);
+
+  var startup = {
+    run: function(settings) {
+      storage.round = 0;
+      storage.times = [];
+      storage.running = true;
+      storage.numRounds = "numRounds" in settings ? settings.numRounds : defaultStorage.numRounds;
+      storage.roundDelay = "roundDelay" in settings ? settings.roundDelay : defaultStorage.roundDelay;
+      storage.deleteFs = "deleteFs" in settings ? settings.deleteFs : defaultStorage.deleteFs;
+      storage.deleteJitCache = "deleteJitCache" in settings ? settings.deleteJitCache : defaultStorage.deleteJitCache;
+      storage.buildBaseline = "buildBaseline" in settings ? settings.buildBaseline : defaultStorage.buildBaseline;
+      if (storage.buildBaseline) {
+        storage.baseline = [];
+      }
       this.runNextRound();
     },
-    get round() {
-      return localStorage["round"] | 0;
-    },
-    set round(value) {
-      localStorage["round"] = value;
-    },
-    get running() {
-      return !!localStorage["running"];
-    },
-    set running(value) {
-      localStorage["running"] = value ? "true" : "";
-    },
-    get times() {
-      return localStorage["times"] ? JSON.parse(localStorage["times"]) : [];
-    },
-    set times(values) {
-      localStorage["times"] = JSON.stringify(values);
-    },
     startTimer: function() {
-      if (!this.running) {
+      if (!storage.running) {
         console.log("startTimer called while benchmark not running");
         return;
       }
       this.startTime = performance.now();
     },
     stopTimer: function() {
-      if (!this.running) {
+      if (!storage.running) {
         console.log("stopTimer called while benchmark not running");
         return;
       }
@@ -54,11 +77,11 @@ var Benchmark = (function() {
       }
       var took = performance.now() - this.startTime;
       this.startTime = null;
-      var times = this.times;
+      var times = storage.times;
       times.push(took);
-      this.times = times;
-      this.round++;
-      if (this.round >= this.numRounds) {
+      storage.times = times;
+      storage.round++;
+      if (storage.round >= storage.numRounds) {
         this.finish();
         return;
       }
@@ -78,38 +101,47 @@ var Benchmark = (function() {
           Components.utils.forceGC();
         }
       }
-      console.log("Scheduling round " + (this.round + 1) + " of " + this.numRounds + " to run in " + this.roundDelay + "ms");
-      setTimeout(run, this.roundDelay);
+      if (storage.deleteFs) {
+        console.log("Deleting fs.");
+        indexedDB.deleteDatabase("asyncStorage");
+      }
+      if (storage.deleteJitCache) {
+        console.log("Deleting jit cache.");
+        indexedDB.deleteJitCache("CompiledMethodCache");
+      }
+      console.log("Scheduling round " + (storage.round + 1) + " of " + storage.numRounds + " to run in " + storage.roundDelay + "ms");
+      setTimeout(run, storage.roundDelay);
     },
     finish: function() {
-      this.running = false;
-      var times = this.times;
+      storage.running = false;
+      var times = storage.times;
       var message = "Current times: " + JSON.stringify(times) + "\n";
-      var baselineMean = mean(this.baseline);
+      var baselineMean = mean(storage.baseline);
       var currentMean = mean(times);
       message += "Current mean : " + Math.round(currentMean) + "ms\n";
-      if (this.baseline.length) {
+      if (storage.baseline.length) {
         message +=
           "Baseline mean: " + Math.round(baselineMean) + "ms\n" +
           "+/-          : " + Math.round(currentMean - baselineMean) + "ms\n" +
           "%            : " + (100 * (currentMean - baselineMean) / baselineMean).toFixed(2) + "\n";
       }
-      if (this.baseline.length) {
-        var p = (this.baseline.length < 2) ? 1 : ttest(this.baseline, times).pValue();
+      if (storage.baseline.length) {
+        var p = (storage.baseline.length < 2) ? 1 : ttest(storage.baseline, times).pValue();
         if (p < 0.05) {
           message += currentMean < baselineMean ? "FASTER" : "SLOWER";
         } else {
           message += "INSIGNIFICANT RESULT";
         }
       }
+      if (storage.buildBaseline) {
+        storage.baseline = times;
+        storage.buildBaseline = false;
+        message = "FINISHED BUILDING BASELINE\n" + message;
+      }
       message = "-------------------------------------------------------------\n" +
                 message + "\n" +
                 "-------------------------------------------------------------\n";
       console.log(message);
-
-      delete localStorage["running"];
-      delete localStorage["times"];
-      delete localStorage["round"];
     }
 
   };
@@ -118,6 +150,42 @@ var Benchmark = (function() {
   startup.startTimer();
 
   return {
+    initUI: function() {
+      var numRoundsEl = document.getElementById("benchmark-num-rounds");
+      var roundDelayEl = document.getElementById("benchmark-round-delay");
+      var deleteFsEl = document.getElementById("benchmark-delete-fs");
+      var deleteJitCacheEl = document.getElementById("benchmark-delete-jit-cache");
+      var startButton = document.getElementById("benchmark-startup-run");
+      var baselineButton = document.getElementById("benchmark-startup-baseline");
+
+      numRoundsEl.value = storage.numRounds;
+      roundDelayEl.value = storage.roundDelay;
+      deleteFsEl.checked = storage.deleteFs;
+      deleteJitCacheEl.checked = storage.deleteJitCache;
+
+      if (storage.baseline.length === 0) {
+        startButton.disabled = true;
+      }
+
+      function getSettings() {
+        return {
+          numRounds: numRoundsEl.value | 0,
+          roundDelay: roundDelayEl.value | 0,
+          deleteFs: !!deleteFsEl.checked,
+          deleteJitCache: !!deleteJitCacheEl.checked,
+        };
+      }
+
+      startButton.onclick = function() {
+        startup.run(getSettings());
+      };
+
+      baselineButton.onclick = function() {
+        var settings = getSettings();
+        settings["buildBaseline"] = true;
+        startup.run(settings);
+      };
+    },
     startup: {
       init: function() {
         var stoppedTimer = false;
