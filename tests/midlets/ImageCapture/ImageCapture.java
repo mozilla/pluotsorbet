6bc270a6e260bd94e550219874d916e958789777
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-02-17 17:43:54 +0100

    Use VideoControl.USE_DIRECT_VIDEO instead of VideoControl.USE_GUI_PRIMITIVE

--- tests/midlets/ImageCapture/ImageCapture.java
+++ tests/midlets/ImageCapture/ImageCapture.java
@@ -6,32 +6,54 @@ import javax.microedition.media.*;
 import javax.microedition.media.control.*;
 import java.io.IOException;
 
-public class ImageCapture extends MIDlet implements CommandListener {
-    private Display display;
-    private Command cmdCapture;
-    private Command cmdExit;
-    private Command cmdBack;
+public class ImageCapture extends MIDlet {
     private Player player;
     private VideoControl videoControl;
-    private Form imageForm;
-    private Image capturedImage;
 
-    public ImageCapture() {
-        display = Display.getDisplay(this);
- 
-        createCamera();
-        createImageForm();       
+    class TestCanvas extends Canvas {
+        private Image capturedImage;
+
+        protected void paint(Graphics g) {
+            g.setColor(0x00FFFFFF);
+            g.fillRect(0, 0, getWidth(), getHeight());
+
+            if (capturedImage != null) {
+                g.drawImage(capturedImage, 0, 0, Graphics.TOP | Graphics.LEFT);
+            } else {
+                videoControl.initDisplayMode(VideoControl.USE_DIRECT_VIDEO, this);
+                try {
+                    videoControl.setDisplayLocation(0, 0);
+                    videoControl.setDisplaySize(getWidth(), getHeight());
+                } catch (MediaException me) {
+                    System.out.println("Unexpected exception: " + me);
+                    me.printStackTrace();
+                }
+
+                videoControl.setVisible(true);
+            }
+        }
+
+        protected void pointerReleased(int x, int y) {
+            try {
+                byte[] imageData;
+
+                imageData = videoControl.getSnapshot("encoding=jpeg");
+
+                capturedImage = Image.createImage(imageData, 0, imageData.length);
+
+                player.close();
+                player = null;
+                videoControl = null;
+
+                this.repaint();
+            } catch (Exception e) {
+                System.out.println("Unexpected exception: " + e);
+                e.printStackTrace();
+            }
+        }
     }
- 
-    private void createCamera() {
-        Form cameraForm = new Form("Camera");
 
-        cmdCapture = new Command("Capture", Command.OK, 0);
-        cmdExit = new Command("Exit", Command.EXIT, 0);
-        cameraForm.addCommand(cmdCapture);
-        cameraForm.addCommand(cmdExit);
-        cameraForm.setCommandListener(this);
-
+    public void startApp() {
         try {
             player = Manager.createPlayer("capture://image");
             player.realize();
@@ -39,70 +61,24 @@ public class ImageCapture extends MIDlet implements CommandListener {
 
             videoControl = (VideoControl)player.getControl("VideoControl");
 
-            Item item = (Item) videoControl.initDisplayMode(VideoControl.USE_GUI_PRIMITIVE, null);
-            cameraForm.append(item);
+            TestCanvas test = new TestCanvas();
+            test.setFullScreenMode(true);
+            Display.getDisplay(this).setCurrent(test);
 
             player.start();
         } catch (Exception e) {
             System.out.println("Unexpected exception: " + e);
             e.printStackTrace();
         }
-
-        display.setCurrent(cameraForm);
     }
 
-    private void createImageForm() {
-        imageForm = new Form("Captured image");
-
-        cmdBack = new Command("Back", Command.BACK, 0);
-        imageForm.addCommand(cmdBack);
-        imageForm.setCommandListener(this);
-    }
-
-    private void captureImage() {
-        try {
-            byte[] imageData;
-
-            imageData = videoControl.getSnapshot("encoding=jpeg");
-
-            capturedImage = Image.createImage(imageData, 0, imageData.length);
-
-            player.close();
-            player = null;
-            videoControl = null;
-
-            imageForm.append(capturedImage);
-            display.setCurrent(imageForm);
-        } catch (Exception e) {
-            System.out.println("Unexpected exception: " + e);
-            e.printStackTrace();
-        }
-    }
- 
-    public void startApp() {
-    }
- 
     public void pauseApp() {
-    }
- 
-    public void exitMIDlet() {
-        notifyDestroyed();
     }
 
     public void destroyApp(boolean unconditional) {
         if (player != null) {
             player.deallocate();
             player.close();
-        }
-    }
-
-    public void commandAction(Command command, Displayable displayable) {
-        if (command == cmdCapture) {
-            captureImage();
-        } else if (command == cmdBack) {
-            createCamera();
-        } else if (command == cmdExit) {
-            notifyDestroyed();
         }
     }
 }
