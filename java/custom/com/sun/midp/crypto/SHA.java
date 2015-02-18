f600c72452d94e08cccdc3234e394636a398a5ae
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-02-18 15:08:35 +0100

    Self-host SHA implementation. Fixes #1109.

--- java/custom/com/sun/midp/crypto/SHA.java
+++ java/custom/com/sun/midp/crypto/SHA.java
@@ -24,30 +24,65 @@
  * information or have any questions.
  */
 
+/* Sha160.java --
+   Copyright (C) 2001, 2002, 2006 Free Software Foundation, Inc.
+
+This file is a part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version.  */
+
 package com.sun.midp.crypto;
+
+import org.mozilla.internal.Sys;
 
 /**
  * Implements the SHA-1 message digest algorithm.
  */
 final class SHA extends MessageDigest {
-    /*
-     * The compute intensive operations are implemented in C
-     * based on the OpenSSL SHA code (from pilotSSLeay). Here we
-     * replicate the state the C code needs.
-     */
+    private static final int BLOCK_SIZE = 64; // inner block size in bytes
 
-    /** State needed for SHA hash. */
-    private int[] state = new int[5]; // h0 ... h4
-    /** State needed for SHA hash. */
-    private int[] num = new int[1];
-    /** State needed for SHA hash. */
-    private int[] count = new int[2]; // Nl and Nh
-    /** State needed for SHA hash. */
-    private int[] data = new int[16]; // data block
+    private static final int[] w = new int[80];
+
+    protected long count;
+
+    private byte[] buffer = new byte[BLOCK_SIZE];
+
+    /** 160-bit interim result. */
+    private int h0, h1, h2, h3, h4;
 
     /** Create SHA digest object. */
-    SHA() {
-        reset();
+    protected SHA() {
+        resetContext();
     }
 
     /**
@@ -70,50 +105,42 @@ final class SHA extends MessageDigest {
      * Resets the MessageDigest to the initial state for further use.
      */
     public void reset() {
-        // SHA1 initialization constants
-        state[0] = 0x67452301;
-        state[1] = 0xEFCDAB89;
-        state[2] = 0x98BADCFE;
-        state[3] = 0x10325476;
-        state[4] = 0xC3D2E1F0;
-        num[0] = 0;
-        count[0] = count[1] = 0;
-        for (int i = 0; i < data.length; i++)
-             data[i] = 0;
+        count = 0L;
+        for (int i = 0; i < BLOCK_SIZE; i++) {
+            buffer[i] = 0;
+        }
+
+        resetContext();
     }
 
     /**
      * Accumulates a hash of the input data. This method is useful when
      * the input data to be hashed is not available in one byte array.
-     * @param inBuf input buffer of data to be hashed
-     * @param inOff offset within inBuf where input data begins
-     * @param inLen length (in bytes) of data to be hashed
+     * @param b input buffer of data to be hashed
+     * @param offset offset within inBuf where input data begins
+     * @param len length (in bytes) of data to be hashed
      * @see #doFinal(byte[], int, int, byte[], int)
      */
-    public void update(byte[] inBuf, int inOff, int inLen) {
-	if (inLen == 0) {
-	    return;
-	}
+    public void update(byte[] b, int offset, int len) {
+	    int n = (int)(count % BLOCK_SIZE);
+        count += len;
+        int partLen = BLOCK_SIZE - n;
+        int i = 0;
 
-        // check parameters to avoid a VM crash
-        int test = inBuf[inOff] + inBuf[inLen - 1] + inBuf[inOff + inLen - 1];
-        nativeUpdate(inBuf, inOff, inLen, state, num, count, data);
+        if (len >= partLen) {
+            Sys.copyArray(b, offset, buffer, n, partLen);
+            transform(buffer, 0);
+            for (i = partLen; i + BLOCK_SIZE - 1 < len; i += BLOCK_SIZE) {
+              transform(b, offset + i);
+            }
+
+            n = 0;
+        }
+
+        if (i < len) {
+            Sys.copyArray(b, offset + i, buffer, n, len - i);
+        }
     }
-
-    /**
-     * Accumulates a hash of the input data. This method is useful when
-     * the input data to be hashed is not available in one byte array.
-     * @param inBuf input buffer of data to be hashed
-     * @param inOff offset within inBuf where input data begins
-     * @param inLen length (in bytes) of data to be hashed
-     * @param state internal hash state
-     * @param num internal hash state
-     * @param count internal hash state
-     * @param data internal hash state
-     */
-    private static native void nativeUpdate(byte[] inBuf, int inOff,
-	     int inLen, int[] state, int[] num, int[] count, int[] data);
-
 
     /**
      * Completes the hash computation by performing final operations
@@ -134,50 +161,139 @@ final class SHA extends MessageDigest {
             throw new DigestException("Buffer too short.");
         }
 
-        // check the parameters to prevent a VM crash
-        int test = buf[offset] + buf[offset + getDigestLength() - 1];
+        byte[] tail = padBuffer(); // pad remaining bytes in buffer
+        update(tail, 0, tail.length); // last transform of a message
+        byte[] result = getResult(); // make a result out of context
 
-        nativeFinal(null, 0, 0, buf, offset, state, num, count, data);
+        reset(); // reset this instance for future re-use
+
+        Sys.copyArray(result, 0, buf, offset, len);
+
         return getDigestLength();
     }
-
-    /**
-     * Generates a hash of all/last input data. Completes and returns the
-     * hash compuatation after performing final operations such as padding.
-     * The MessageDigest object is reset after this call.
-     * @param inBuf input buffer of data to be hashed
-     * @param inOff offset within inBuf where input data begins
-     * @param inLen length (in bytes) of data to be hashed
-     * @param outBuf output buffer where the hash should be placed
-     * @param outOff offset within outBuf where the resulting hash begins
-     * @param state internal hash state
-     * @param num internal hash state
-     * @param count internal hash state
-     * @param data internal hash state
-     */
-    private static native void nativeFinal(byte[] inBuf, int inOff,
-		    int inLen, byte[] outBuf, int outOff,
-		    int[] state, int[] num, int[] count, int[] data);
 
     /**
      * Clones the MessageDigest object.
      * @return a clone of this object
      */
     public Object clone() {
-	SHA cpy = new SHA();
+        SHA cpy = new SHA();
 
-	System.arraycopy(this.state, 0, cpy.state, 0, this.state.length);
-	System.arraycopy(this.num, 0, cpy.num, 0, this.num.length);
-	System.arraycopy(this.count, 0, cpy.count, 0, this.count.length);
-	System.arraycopy(this.data, 0, cpy.data, 0, this.data.length);
+        cpy.h0 = this.h0;
+        cpy.h1 = this.h1;
+        cpy.h2 = this.h2;
+        cpy.h3 = this.h3;
+        cpy.h4 = this.h4;
+        cpy.count = this.count;
+        Sys.copyArray(this.buffer, 0, cpy.buffer, 0, this.buffer.length);
 
-	nativeClone(cpy.data);
-
-	return cpy;
+        return cpy;
     }
 
-    /**
-     * Does the additional work needed to clone the object natively.
-     */
-    private static native void nativeClone(int[] data);
+    protected void transform(byte[] in, int offset) {
+        int[] result = sha(h0, h1, h2, h3, h4, in, offset);
+        h0 = result[0];
+        h1 = result[1];
+        h2 = result[2];
+        h3 = result[3];
+        h4 = result[4];
+    }
+
+    protected byte[] padBuffer() {
+        int n = (int)(count % BLOCK_SIZE);
+        int padding = (n < 56) ? (56 - n) : (120 - n);
+        byte[] result = new byte[padding + 8];
+        // padding is always binary 1 followed by binary 0s
+        result[0] = (byte) 0x80;
+        // save number of bits, casting the long to an array of 8 bytes
+        long bits = count << 3;
+        result[padding++] = (byte)(bits >>> 56);
+        result[padding++] = (byte)(bits >>> 48);
+        result[padding++] = (byte)(bits >>> 40);
+        result[padding++] = (byte)(bits >>> 32);
+        result[padding++] = (byte)(bits >>> 24);
+        result[padding++] = (byte)(bits >>> 16);
+        result[padding++] = (byte)(bits >>> 8);
+        result[padding  ] = (byte) bits;
+        return result;
+    }
+
+    protected byte[] getResult() {
+        return new byte[] {
+            (byte)(h0 >>> 24), (byte)(h0 >>> 16), (byte)(h0 >>> 8), (byte) h0,
+            (byte)(h1 >>> 24), (byte)(h1 >>> 16), (byte)(h1 >>> 8), (byte) h1,
+            (byte)(h2 >>> 24), (byte)(h2 >>> 16), (byte)(h2 >>> 8), (byte) h2,
+            (byte)(h3 >>> 24), (byte)(h3 >>> 16), (byte)(h3 >>> 8), (byte) h3,
+            (byte)(h4 >>> 24), (byte)(h4 >>> 16), (byte)(h4 >>> 8), (byte) h4 };
+    }
+
+    protected void resetContext() {
+        // magic SHA-1/RIPEMD160 initialisation constants
+        h0 = 0x67452301;
+        h1 = 0xEFCDAB89;
+        h2 = 0x98BADCFE;
+        h3 = 0x10325476;
+        h4 = 0xC3D2E1F0;
+    }
+
+    private static synchronized final int[] sha(int hh0, int hh1, int hh2,
+                                                int hh3, int hh4, byte[] in,
+                                                int offset) {
+        int A = hh0;
+        int B = hh1;
+        int C = hh2;
+        int D = hh3;
+        int E = hh4;
+        int r, T;
+
+        for (r = 0; r < 16; r++) {
+            w[r] =  in[offset++]         << 24 |
+                    (in[offset++] & 0xFF) << 16 |
+                    (in[offset++] & 0xFF) << 8 |
+                    (in[offset++] & 0xFF);
+        }
+
+        for (r = 16; r < 80; r++) {
+            T = w[r - 3] ^ w[r - 8] ^ w[r - 14] ^ w[r - 16];
+            w[r] = T << 1 | T >>> 31;
+        }
+
+        for (r = 0; r < 20; r++) { // rounds 0-19
+            T = (A << 5 | A >>> 27) + ((B & C) | (~B & D)) + E + w[r] + 0x5A827999;
+            E = D;
+            D = C;
+            C = B << 30 | B >>> 2;
+            B = A;
+            A = T;
+        }
+
+        for (r = 20; r < 40; r++) { // rounds 20-39
+            T = (A << 5 | A >>> 27) + (B ^ C ^ D) + E + w[r] + 0x6ED9EBA1;
+            E = D;
+            D = C;
+            C = B << 30 | B >>> 2;
+            B = A;
+            A = T;
+        }
+
+        for (r = 40; r < 60; r++) { // rounds 40-59
+            T = (A << 5 | A >>> 27) + (B & C | B & D | C & D) + E + w[r] + 0x8F1BBCDC;
+            E = D;
+            D = C;
+            C = B << 30 | B >>> 2;
+            B = A;
+            A = T;
+        }
+
+        for (r = 60; r < 80; r++) { // rounds 60-79
+            T = (A << 5 | A >>> 27) + (B ^ C ^ D) + E + w[r] + 0xCA62C1D6;
+            E = D;
+            D = C;
+            C = B << 30 | B >>> 2;
+            B = A;
+            A = T;
+        }
+
+        return new int[] { hh0 + A, hh1 + B, hh2 + C, hh3 + D, hh4 + E };
+    }
 }
