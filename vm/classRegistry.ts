36a0b56f4c893df3224a742f0dbef75b308e3faa
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-03-06 13:47:13 +0100

    Remove duplicate class from the classNames array

--- vm/classRegistry.ts
+++ vm/classRegistry.ts
@@ -62,7 +62,6 @@ module J2ME {
        * because they don't have any static state.
        */
       var classNames = [
-        "com/sun/cldc/isolate/Isolate", // Not used frequently, but needs setup before we start the isolates.
         "java/lang/Integer",
         "java/lang/Character",
         "java/lang/Math",
