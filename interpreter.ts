64b48162843eaab0b11415e64b11c4ad6f0891b2
Myk Melez <myk@mozilla.org>
2015-03-06 16:04:31 -0800

    describe way to reduce number of methods we link

--- interpreter.ts
+++ interpreter.ts
@@ -1134,9 +1134,16 @@ module J2ME {
             // Resolve method and do the class init check if necessary.
             var calleeMethodInfo = resolveMethod(index, mi.classInfo, isStatic);
 
+            // TODO: the only thing we really need to do here is set *state*
+            // to MethodState.Compiled if the method has a native implementation,
+            // so we should do something like that instead to reduce the number
+            // of methods we link.  We just need to create a better call for it.
             if (calleeMethodInfo.fn && calleeMethodInfo.fn.name === "lazyFn") {
               linkKlassMethod(calleeMethodInfo, true);
             }
+            // if (findNativeMethodImplementation(calleeMethodInfo)) {
+            //   calleeMethodInfo.state = MethodState.Compiled;
+            // }
 
             // Fast path for some of the most common interpreter call targets.
             if (calleeMethodInfo.implKey === "java/lang/Object.<init>.()V") {
