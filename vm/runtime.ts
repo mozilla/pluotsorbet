2bb301adc2908aa8d8e2b0c3bf63302fa6f2f590
Brendan Dahl <brendan.dahl@gmail.com>
2015-03-18 15:59:38 -0700

    Change executeFrames to only execute one frame.

--- vm/runtime.ts
+++ vm/runtime.ts
@@ -1280,7 +1280,7 @@ module J2ME {
         for (var i = 0; i < slots; i++) {
           frame.setLocal(j++, arguments[i]);
         }
-        return $.ctx.executeFrames([frame]);
+        return $.ctx.executeFrame(frame);
       };
       (<any>method).methodInfo = methodInfo;
       return method;
@@ -1313,7 +1313,7 @@ module J2ME {
           return;
         }
       }
-      return $.ctx.executeFrames([frame]);
+      return $.ctx.executeFrame(frame);
     };
     (<any>method).methodInfo = methodInfo;
     return method;
