a42fcf93c22a3a1d9c8c577ae6e623ef152e42b9
Myk Melez <myk@mozilla.org>
2015-03-21 01:50:31 -0700

    add benchmark for System.out.print(ln)

---
+++ bench/SystemOutBench.java
@@ -0,0 +1,29 @@
+package benchmark;
+
+import com.sun.cldchi.jvm.JVM;
+
+public class SystemOutBench {
+  void runBenchmark() {
+    long start, time;
+
+    start = JVM.monotonicTimeMillis();
+    for (int i = 0; i < 1000; i++) {
+      System.out.println("I am the very model of a modern major general.");
+    }
+
+    for (int i = 0; i < 100; i++) {
+      for (int j = 0; j < 100; j++) {
+        System.out.print(j % 10);
+      }
+      System.out.println();
+    }
+
+    time = JVM.monotonicTimeMillis() - start;
+    System.out.println("Time: " + time);
+  }
+
+  public static void main(String args[]) {
+    SystemOutBench bench = new SystemOutBench();
+    bench.runBenchmark();
+  }
+}
