130e1cda2329d38cd960a6bc3311f8e2dfbaca89
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-03-23 12:23:35 +0100

    Fix indentation

--- tests/com/sun/cldc/i18n/UTF_8_Writer_sizeOf.java
+++ tests/com/sun/cldc/i18n/UTF_8_Writer_sizeOf.java
@@ -9,27 +9,26 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class UTF_8_Writer_sizeOf implements Testlet {
-  public int getExpectedPass() { return 1; }
-  public int getExpectedFail() { return 0; }
-  public int getExpectedKnownFail() { return 0; }
+    public int getExpectedPass() { return 1; }
+    public int getExpectedFail() { return 0; }
+    public int getExpectedKnownFail() { return 0; }
 
-  public void test (TestHarness th)
-  {
-    try {
-        String str = "abcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZabcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZ";
-        char[] cbuf = new char[str.length()];
-        str.getChars(0, str.length(), cbuf, 0);
-		    
-		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        StreamWriter sw = new UTF_8_Writer();
+    public void test (TestHarness th) {
+        try {
+            String str = "abcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZabcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZ";
+            char[] cbuf = new char[str.length()];
+            str.getChars(0, str.length(), cbuf, 0);
 
-        sw.open(baos, "UTF_8");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            StreamWriter sw = new UTF_8_Writer();
 
-        th.check(sw.sizeOf(cbuf, 0, cbuf.length), 84);
+            sw.open(baos, "UTF_8");
 
-    		sw.close ();
-    } catch (IOException e) {
-	      th.fail("Unexpected exception: " + e);
+            th.check(sw.sizeOf(cbuf, 0, cbuf.length), 84);
+
+            sw.close();
+        } catch (IOException e) {
+            th.fail("Unexpected exception: " + e);
+        }
     }
-  }
 }
