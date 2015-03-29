e49e41d59bcf33b0fc00e7220b2ab6045ef1b25c
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-03-29 19:37:08 +0200

    Test opening a localmsg connection in a Foreground MIDlet both before and after starting it

--- tests/midlets/background/ForegroundMIDlet.java
+++ tests/midlets/background/ForegroundMIDlet.java
@@ -1,12 +1,23 @@
 package tests.background;
 
+import java.io.IOException;
 import javax.microedition.midlet.MIDlet;
+import javax.microedition.io.Connector;
+import com.nokia.mid.s40.io.LocalMessageProtocolConnection;
 
 public class ForegroundMIDlet extends MIDlet {
     public ForegroundMIDlet() {
     }
 
     public void startApp() {
+        try {
+            LocalMessageProtocolConnection client = (LocalMessageProtocolConnection)Connector.open("localmsg://mozilla");
+        } catch (IOException e) {
+            System.out.println("Unexpected exception: " + e);
+            e.printStackTrace();
+            return;
+        }
+
         System.out.println("Hello World from foreground MIDlet");
 
         if (System.getProperty("prop1") != null && System.getProperty("prop2") != null) {
