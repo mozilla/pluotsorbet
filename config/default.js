77e8e3db17421fb40661a364fff6eec9600d2dc6
Myk Melez <myk@mozilla.org>
2015-04-03 13:28:08 -0700

    correct syntax

--- config/default.js
+++ config/default.js
@@ -29,6 +29,6 @@ var config = {
 
 // The base directory of the app, relative to the current page.  Normally this
 // is the directory from which the page was loaded, but some test pages load
-// from a subdirectory, like tests/fs/, and they set this accordingly such that
-// code loads files, like libs/fs-init.js, can load them from the right place.
+// from a subdirectory, like tests/fs/, and they set this accordingly, so code
+// that loads files, like libs/fs-init.js, can load them from the right place.
 var APP_BASE_DIR = "./";
