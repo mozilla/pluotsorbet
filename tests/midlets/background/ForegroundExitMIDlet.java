73938f95a3a992016b66de5e04722b00ee5a7fae
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-04-09 00:18:48 +0200

    Add TODO comment

--- tests/midlets/background/ForegroundExitMIDlet.java
+++ tests/midlets/background/ForegroundExitMIDlet.java
@@ -5,6 +5,8 @@ import com.sun.cldc.isolate.Isolate;
 
 public class ForegroundExitMIDlet extends MIDlet {
     public void startApp() {
+        // TODO: Find a better way to do this, with a public J2ME function
+        // and not an internal one.
         Isolate.currentIsolate().exit(0);
     }
 
