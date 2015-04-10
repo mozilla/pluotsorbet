19f6196265823c7583ace75c00cdeeed3caa7de0
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-04-10 17:26:36 +0200

    Allow MIDlets to set a maximum length for the SMS verification input field

--- midp/sms.js
+++ midp/sms.js
@@ -58,6 +58,15 @@ function promptForMessageText() {
     input.addEventListener('input', function() {
         btnDone.disabled = (input.value.length === 0);
     });
+    if (MIDlet.SMSDialogInputMaxLength) {
+      input.onkeydown = function(e) {
+        if (input.value.length >= MIDlet.SMSDialogInputMaxLength) {
+          return e.keyCode !== 0 && !util.isPrintable(e.keyCode);
+        }
+
+        return true;
+      }
+    }
 
     btnCancel.addEventListener('click', function() {
         console.warn('SMS prompt canceled.');
