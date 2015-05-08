4278e3b5105408c3a32c3c6bfecd0051ea8fb5af
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-05-08 22:57:37 +0200

    LCDUIUtils functions should be public

--- java/custom/com/nokia/mid/ui/lcdui/LCDUIUtils.java
+++ java/custom/com/nokia/mid/ui/lcdui/LCDUIUtils.java
@@ -5,26 +5,26 @@ import javax.microedition.lcdui.Displayable;
 import com.nokia.mid.ui.lcdui.DisplayStateListener;
 
 public class LCDUIUtils {
-    static java.lang.Object getObjectTrait(java.lang.Object target, java.lang.String traitName) {
+    public static java.lang.Object getObjectTrait(java.lang.Object target, java.lang.String traitName) {
         System.out.println("LCDUIUtils::getObjectTrait(Object,String) not implemented");
         return null;
     }
 
-    static boolean setObjectTrait(java.lang.Object target, java.lang.String traitName, java.lang.Object value) {
+    public static boolean setObjectTrait(java.lang.Object target, java.lang.String traitName, java.lang.Object value) {
         System.out.println("LCDUIUtils::setObjectTrait(Object,String) not implemented");
         return false;
     }
 
-    static boolean isDisplayActive(Display display) {
+    public static boolean isDisplayActive(Display display) {
         System.out.println("LCDUIUtils::isDisplayActive(Display) not implemented");
         return true;
     }
 
-    static void setDisplayStateListener(Display display, DisplayStateListener listener) {
+    public static void setDisplayStateListener(Display display, DisplayStateListener listener) {
         System.out.println("LCDUIUtils::setDisplayStateListener(Dispay,DisplayStateListener) not implemented");
     }
 
-    static void setCurrent(Display display, Displayable displayable, String str) {
+    public static void setCurrent(Display display, Displayable displayable, String str) {
         System.out.println("LCDUIUtil::setCurrent no implemente.");
     }
 }
