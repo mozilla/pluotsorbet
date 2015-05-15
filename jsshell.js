e2dd057273def557345eaec649f6304500641af0
Michael Bebenita <mbebenita@gmail.com>
2015-05-14 17:49:29 -0700

    Merge.

--- jsshell.js
+++ jsshell.js
@@ -162,7 +162,7 @@ try {
   }
   load("libs/relooper.js", "libs/native.js", "bld/j2me.js","libs/zipfile.js", "blackBox.js",
     "libs/encoding.js", "util.js", "libs/jarstore.js",
-    "native.js", "midp/midp.js"
+    "native.js", "midp/midp.js",
     "libs/long.js", "midp/crypto.js", "libs/forge/md5.js", "libs/forge/util.js"
     // "bld/classes.jar.js"
   );
