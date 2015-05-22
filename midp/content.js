a5f90f64bd710a30e2e87ed44282c0932bd056ac
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-05-22 15:44:47 +0200

    Use addUnimplementedNative for unimplemented natives

--- midp/content.js
+++ midp/content.js
@@ -4,10 +4,7 @@
 'use strict';
 
 var Content = (function() {
-  Native["com/sun/j2me/content/RegistryStore.init.()Z"] = function() {
-    console.warn("com/sun/j2me/content/RegistryStore.init.()Z not implemented");
-    return 1;
-  };
+  addUnimplementedNative("com/sun/j2me/content/RegistryStore.init.()Z", 1);
 
   Native["com/sun/j2me/content/RegistryStore.forSuite0.(I)Ljava/lang/String;"] = function(suiteID) {
     console.warn("com/sun/j2me/content/RegistryStore.forSuite0.(I)Ljava/lang/String; not implemented");
@@ -15,9 +12,7 @@ var Content = (function() {
   };
 
   addUnimplementedNative("com/sun/j2me/content/RegistryStore.findHandler0.(Ljava/lang/String;ILjava/lang/String;)Ljava/lang/String;", null);
-
   addUnimplementedNative("com/sun/j2me/content/RegistryStore.register0.(ILjava/lang/String;Lcom/sun/j2me/content/ContentHandlerRegData;)Z", 0);
-
   addUnimplementedNative("com/sun/j2me/content/RegistryStore.getHandler0.(Ljava/lang/String;Ljava/lang/String;I)Ljava/lang/String;", null);
 
   Native["com/sun/j2me/content/AppProxy.isInSvmMode.()Z"] = function() {
@@ -25,9 +20,10 @@ var Content = (function() {
     return 0;
   };
 
+  addUnimplementedNative("com/sun/j2me/content/AppProxy.midletIsRemoved.(ILjava/lang/String;)V");
+
   addUnimplementedNative("com/sun/j2me/content/InvocationStore.setCleanup0.(ILjava/lang/String;Z)V");
   addUnimplementedNative("com/sun/j2me/content/InvocationStore.get0.(Lcom/sun/j2me/content/InvocationImpl;ILjava/lang/String;IZ)I", 0);
   addUnimplementedNative("com/sun/j2me/content/InvocationStore.getByTid0.(Lcom/sun/j2me/content/InvocationImpl;II)I", 0);
   addUnimplementedNative("com/sun/j2me/content/InvocationStore.resetFlags0.(I)V");
-  addUnimplementedNative("com/sun/j2me/content/AppProxy.midletIsRemoved.(ILjava/lang/String;)V");
 })();
