088647f658ed207dbb1e3f622f37f7f58e7590ed
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-06-04 20:10:53 +0200

    Remove unused variable

--- bench/BouncyCastleSHA256.java
+++ bench/BouncyCastleSHA256.java
@@ -4,13 +4,11 @@ import com.sun.cldchi.jvm.JVM;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 
 public class BouncyCastleSHA256 {
-
+    // Try to mimic a popular midlet that commonly does around
+    // 170 update calls on 4096 bytes at a time.
     private static final int UPDATES = 170;
 
     public static void main(String args[]) {
-        // Try to mimic a popular midlet that commonly does around
-        // 170 update calls on 4096 bytes at a time.
-        int updates = 170;
         byte[] digest = new byte[4096];
         for (int i = 0; i < digest.length; i++) {
             digest[i] = (byte)i;
@@ -26,6 +24,6 @@ public class BouncyCastleSHA256 {
             digester.doFinal(retValue, 0);
         }
         long time = JVM.monotonicTimeMillis() - start;
-        System.out.println("SHA256: " + time);
+        System.out.println("BouncyCastleSHA256: " + time);
     }
 }
