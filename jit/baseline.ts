d43788dc975cfde1ab151170bcbde7b30ad9ac43
Michael Bebenita <mbebenita@gmail.com>
2015-06-11 13:26:17 -0700

    Simplify get/put field ops.

--- jit/baseline.ts
+++ jit/baseline.ts
@@ -801,42 +801,21 @@ module J2ME {
       this.blockEmitter.writeLn("return " + this.pop(kind) + ";");
     }
 
-    getField(object: string, field: FieldInfo) {
-      var base = object + "._address";
-      switch (field.kind) {
-        case Kind.Reference:
-          return "ref[" + base + "+" + field.byteOffset + ">>2]";
-        case Kind.Int:
-        case Kind.Byte:
-        case Kind.Char:
-        case Kind.Short:
-        case Kind.Boolean:
-          return "i32[" + base + "+" + field.byteOffset + ">>2]";
-          break;
-        case Kind.Float:
-          return "f32[" + base + "+" + field.byteOffset + ">>2]";
-          break;
-        //case Kind.Long:
-        //  setter = new Function("value", "i32[this._address + " + field.byteOffset + " >> 2] = J2ME.numberToLong(value); i32[this._address + 4 + " + field.byteOffset + " >> 2] = tempReturn0;");
-        //  getter = new Function("return J2ME.longToNumber(i32[this._address + " + field.byteOffset + " >> 2], i32[this._address + 4 + " + field.byteOffset + " >> 2]);");
-        //  break;
-        //case Kind.Double:
-        //  setter = new Function("value", "aliasedF64[0] = value; i32[this._address + " + field.byteOffset + " >> 2] = aliasedI32[0]; i32[this._address + 4 + " + field.byteOffset + " >> 2] = aliasedI32[1];");
-        //  getter = new Function("aliasedI32[0] = i32[this._address + " + field.byteOffset + " >> 2];  aliasedI32[1] = i32[this._address + 4 + " + field.byteOffset + " >> 2]; return aliasedF64[0];");
-        //  break;
-        default:
-          Debug.assert(false, Kind[field.kind]);
-          break;
-      }
-    }
-
     emitGetField(fieldInfo: FieldInfo, isStatic: boolean) {
       if (isStatic) {
         this.emitClassInitializationCheck(fieldInfo.classInfo);
       }
       var kind = getSignatureKind(fieldInfo.utf8Signature);
       var object = isStatic ? this.runtimeClass(fieldInfo.classInfo) : this.pop(Kind.Reference);
-      this.emitPush(kind, this.getField(object, fieldInfo), Precedence.Member);
+      var address = object + "._address+" + fieldInfo.byteOffset;
+      if (fieldInfo.kind === Kind.Reference) {
+        this.emitPush(kind, "ref[" + address + ">>2]", Precedence.Member);
+      } else {
+        this.emitPush(Kind.Int, "i32[" + address + ">>2]", Precedence.Member);
+        if (isTwoSlot(fieldInfo.kind)) {
+          this.emitPush(Kind.Int, "i32[" + address + "+4>>2]", Precedence.Member);
+        }
+      }
     }
 
     emitPutField(fieldInfo: FieldInfo, isStatic: boolean) {
@@ -844,9 +823,20 @@ module J2ME {
         this.emitClassInitializationCheck(fieldInfo.classInfo);
       }
       var kind = getSignatureKind(fieldInfo.utf8Signature);
-      var value = this.pop(kind, Precedence.Sequence);
+      var l = this.pop(Kind.Int, Precedence.Sequence), h = "";
+      if (isTwoSlot(kind)) {
+        h = this.pop(Kind.Int, Precedence.Sequence);
+      }
       var object = isStatic ? this.runtimeClass(fieldInfo.classInfo) : this.pop(Kind.Reference);
-      this.blockEmitter.writeLn(this.getField(object, fieldInfo) + "=" + value + ";");
+      var address = object + "._address+" + fieldInfo.byteOffset;
+      if (fieldInfo.kind === Kind.Reference) {
+        this.blockEmitter.writeLn("ref[" + address + ">>2]=" + l + ";");
+      } else {
+        this.blockEmitter.writeLn("i32[" + address + ">>2]=" + l + ";");
+        if (isTwoSlot(fieldInfo.kind)) {
+          this.blockEmitter.writeLn("i32[" + address + "+$>>2]=" + h + ";");
+        }
+      }
     }
 
     setBlockStackHeight(pc: number, height: number) {
