4d08cbd10f8c2b873f6139e591ca5b604f4c0d2d
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-06-15 19:50:32 +0200

    Add benchmarks for readInt and readShort

--- bench/DataInputOutputStreamBench.java
+++ bench/DataInputOutputStreamBench.java
@@ -13,7 +13,7 @@ public class DataInputOutputStreamBench {
   void writeUTF(OutputStream out) throws IOException {
       DataOutputStream dos = new DataOutputStream(out);
 
-      for (int i = 0; i < 50000; i++) {
+      for (int i = 0; i < 70000; i++) {
           dos.writeUTF("abcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZ");
       }
 
@@ -26,6 +26,26 @@ public class DataInputOutputStreamBench {
 
       for (int i = 0; i < 25000; i++) {
           String str = dis.readUTF();
+      }
+
+      dis.close();
+  }
+
+  void readInt(InputStream is) throws IOException {
+      DataInputStream dis = new DataInputStream(is);
+
+      for (int i = 0; i < 200000; i++) {
+          int aInt = dis.readInt();
+      }
+
+      dis.close();
+  }
+
+  void readShort(InputStream is) throws IOException {
+      DataInputStream dis = new DataInputStream(is);
+
+      for (int i = 0; i < 400000; i++) {
+          int aInt = dis.readShort();
       }
 
       dis.close();
@@ -45,6 +65,14 @@ public class DataInputOutputStreamBench {
           start = JVM.monotonicTimeMillis();
           readUTF(in);
           System.out.println("DataInputStream::readUTF: " + (JVM.monotonicTimeMillis() - start));
+
+          start = JVM.monotonicTimeMillis();
+          readInt(in);
+          System.out.println("DataInputStream::readInt: " + (JVM.monotonicTimeMillis() - start));
+
+          start = JVM.monotonicTimeMillis();
+          readShort(in);
+          System.out.println("DataInputStream::readShort: " + (JVM.monotonicTimeMillis() - start));
       } catch (IOException e) {
           System.out.println("Unexpected exception: " + e);
           e.printStackTrace();
