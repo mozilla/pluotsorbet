58594510d96400029c31f89fc2a461b7400f580c
Myk Melez <myk@mozilla.org>
2015-06-18 16:17:48 -0700

    add build configuration for RunWhatsApp and RunWeChat (preview) targets

---
+++ config/runwechatpreview.js
@@ -0,0 +1,10 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+MIDlet.name = "WeChat";
+
+config.downloadJAD = "http://dldir1.qq.com/foxmail/AshaWeChat.jad";
+config.midletClassName = "com.tencent.mm.ui.WeChat";
+config.platform = "Nokia311";
