df4bf5f3a8dd729cddf494a62b37e7a79f82096b
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-02 17:42:57 +0200

    Fix merge problems

--- vm/context.ts
+++ vm/context.ts
@@ -337,7 +337,7 @@ module J2ME {
 
       ctx.nativeThread.pushFrame(null);
       ctx.nativeThread.pushFrame(sys.getMethodByNameString("isolate0Entry", "(Ljava/lang/String;[Ljava/lang/String;)V"));
-      ctx.nativeThread.frame.setParameter(Kind.Reference, 0, J2ME.newString(className.replace(/\./g, "/"))._address);
+      ctx.nativeThread.frame.setParameter(Kind.Reference, 0, J2ME.newString(className.replace(/\./g, "/")));
       ctx.nativeThread.frame.setParameter(Kind.Reference, 1, arrayAddr);
       ctx.start();
       release || Debug.assert(!U, "Unexpected unwind during isolate initialization.");
