b4faa8b241023bd54d2c8777c5b7000432826da1
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-22 21:22:01 +0200

    Add a test where the garbage collectors clears a WeakReference; test holding two weak references to a single object

--- tests/java/lang/ref/TestWeakReference.java
+++ tests/java/lang/ref/TestWeakReference.java
@@ -1,18 +1,50 @@
 package java.lang.ref;
 
+import org.mozilla.internal.Sys;
 import gnu.testlet.Testlet;
 import gnu.testlet.TestHarness;
 
 public class TestWeakReference implements Testlet {
-  public int getExpectedPass() { return 2; }
+  public int getExpectedPass() { return 8; }
   public int getExpectedFail() { return 0; }
   public int getExpectedKnownFail() { return 0; }
 
+  int objAddr;
+  WeakReference gcWeakRef;
+
+  class SetWeakRefThread extends Thread {
+    public void run() {
+      Object obj = new Object();
+      gcWeakRef = new WeakReference(obj);
+    }
+  }
+
   public void test(TestHarness th) {
     Object obj = new Object();
+
     WeakReference weakRef = new WeakReference(obj);
+    WeakReference weakRef2 = new WeakReference(obj);
     th.check(weakRef.get(), obj, "weakly held referent is object");
+    th.check(weakRef2.get(), obj, "second weakly held referent is object");
     weakRef.clear();
-    th.check(weakRef.get(), null, "cleared weakly held referent is null");
+    th.check(weakRef.get() == null, "cleared weakly held referent is null");
+    weakRef.clear();
+    th.check(weakRef.get() == null, "clearing a cleared WeakReference works");
+    th.check(weakRef2.get(), obj, "second weakly held referent is object");
+
+    SetWeakRefThread thread = new SetWeakRefThread();
+    thread.start();
+    try {
+        thread.join();
+    } catch (InterruptedException e) {
+      th.fail("Unexpected exception: " + e);
+    }
+
+    th.check(gcWeakRef.get() != null, "weakly held referent isn't null");
+    Sys.forceCollection();
+    Sys.forceCollection();
+    th.check(gcWeakRef.get() == null, "GC cleared weakly held referent is null");
+    gcWeakRef.clear();
+    th.check(weakRef.get() == null, "clearing a WeakReference cleared by the GC works");
   }
 }
