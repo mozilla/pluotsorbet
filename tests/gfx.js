1850d69982bf4a9ef567c37117cd36e3df17d675
Myk Melez <myk@mozilla.org>
2015-07-24 10:30:49 -0700

    update max different pixels on Mac for DrawAndFillRoundRectTest

--- tests/gfx.js
+++ tests/gfx.js
@@ -35,7 +35,7 @@ var gfxTests = [
   { name: "gfx/CanvasWithHeaderTest", maxDifferentLinux: 823, maxDifferentMac: 1351 },
   { name: "gfx/ImageRenderingTest", maxDifferentLinux: 0, maxDifferentMac: 22113 },
   { name: "gfx/FillRectTest", maxDifferentLinux: 0, maxDifferentMac: 756 },
-  { name: "gfx/DrawAndFillRoundRectTest", maxDifferentLinux: 243, maxDifferentMac: 1592 },
+  { name: "gfx/DrawAndFillRoundRectTest", maxDifferentLinux: 243, maxDifferentMac: 2244 },
   { name: "gfx/DrawAndFillArcTest", maxDifferentLinux: 5, maxDifferentMac: 1765 },
   { name: "gfx/DrawStringTest", maxDifferentLinux: 232, maxDifferentMac: 321 },
   { name: "gfx/DrawRedStringTest", maxDifferentLinux: 338, maxDifferentMac: 485 },
