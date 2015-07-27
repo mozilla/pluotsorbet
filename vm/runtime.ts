f9348ec5827a1fca77712c23092605be7e8999a2
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-27 03:29:08 +0200

    Don't use getArrayFromAddr in fromStringAddr

--- vm/runtime.ts
+++ vm/runtime.ts
@@ -1426,15 +1426,25 @@ module J2ME {
     return newArray(PrimitiveClassInfo.I, size);
   }
 
+  var jStringDecoder = new TextDecoder('utf-16');
+
+  export function fromJavaChars(charsAddr, offset = 0, count = i32[charsAddr + Constants.ARRAY_LENGTH_OFFSET >> 2]) {
+    release || assert(charsAddr !== Constants.NULL);
+
+    var start = (Constants.ARRAY_HDR_SIZE + charsAddr >> 1) + offset;
+
+    return jStringDecoder.decode(u16.subarray(start, start + count));
+  }
+
   export function fromStringAddr(stringAddr: number): string {
     if (stringAddr === Constants.NULL) {
       return null;
     }
 
     // XXX Retrieve the characters directly from memory, without indirecting
-    // through getHandle and getArrayFromAddr.
+    // through getHandle.
     var javaString = <java.lang.String>getHandle(stringAddr);
-    return util.fromJavaChars(getArrayFromAddr(javaString.value), javaString.offset, javaString.count);
+    return fromJavaChars(javaString.value, javaString.offset, javaString.count);
   }
 
   export function checkDivideByZero(value: number) {
