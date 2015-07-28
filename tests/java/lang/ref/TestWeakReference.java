d6d668c4d61361ba848bd3c76cb64c3ad5c5446b
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-28 18:24:53 +0200

    Test multiple WeakReferences to the same object; add a complex WeakReference::clear test

--- tests/java/lang/ref/TestWeakReference.java
+++ tests/java/lang/ref/TestWeakReference.java
@@ -10,13 +10,19 @@ public class TestWeakReference implements Testlet {
 
   int objAddr;
   WeakReference gcWeakRef;
+  WeakReference gcWeakRef2;
 
   class SetWeakRefThread extends Thread {
     public void run() {
       Object obj = new Object();
       gcWeakRef = new WeakReference(obj);
+      gcWeakRef2 = new WeakReference(obj);
     }
   }
+
+  native void setNative(Object object);
+  native boolean checkNative(Object object);
+  native void simulateFinalizer(Object object);
 
   public void test(TestHarness th) {
     Object obj = new Object();
@@ -31,6 +37,7 @@ public class TestWeakReference implements Testlet {
     th.check(weakRef.get() == null, "clearing a cleared WeakReference works");
     th.check(weakRef2.get(), obj, "second weakly held referent is object");
 
+
     SetWeakRefThread thread = new SetWeakRefThread();
     thread.start();
     try {
@@ -40,10 +47,27 @@ public class TestWeakReference implements Testlet {
     }
 
     th.check(gcWeakRef.get() != null, "weakly held referent isn't null");
+    th.check(gcWeakRef2.get() != null, "second weakly held referent isn't null");
     Runtime.getRuntime().gc();
     Runtime.getRuntime().gc();
     th.check(gcWeakRef.get() == null, "GC cleared weakly held referent is null");
+    th.check(gcWeakRef2.get() == null, "second GC cleared weakly held referent is null");
     gcWeakRef.clear();
-    th.check(weakRef.get() == null, "clearing a WeakReference cleared by the GC works");
+    th.check(gcWeakRef.get() == null, "clearing a WeakReference cleared by the GC works");
+
+
+    WeakReference clearWeakRef = new WeakReference(obj);
+    clearWeakRef.clear();
+
+    // Simulate the situation where a native is attached to an object allocated
+    // at the same address as a WeakReference object. In theory this could happen
+    // after a WeakReference object is garbage collected.
+    setNative(clearWeakRef);
+
+    simulateFinalizer(obj);
+
+    // Check that the native object is still in the NativeMap. If we don't clear
+    // the WeakReference correctly, this could fail.
+    th.check(checkNative(clearWeakRef), "native object still exists");
   }
 }
