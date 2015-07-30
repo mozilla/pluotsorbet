1ad0d2f01501782719e2185b39f2a1b0bdd93e1f
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-30 22:57:24 +0200

    Collect everything before running the test

--- tests/org/mozilla/gc/TestRuntime.java
+++ tests/org/mozilla/gc/TestRuntime.java
@@ -8,10 +8,20 @@ public class TestRuntime implements Testlet {
     public int getExpectedFail() { return 0; }
     public int getExpectedKnownFail() { return 0; }
 
+    void collectAll() {
+        long freeMemory;
+        long endFreeMemory;
+        do {
+            freeMemory = Runtime.getRuntime().freeMemory();
+            Runtime.getRuntime().gc();
+            endFreeMemory = Runtime.getRuntime().freeMemory();
+        } while (endFreeMemory > freeMemory);
+    }
+
     public void test(TestHarness th) {
         System.out.println("freeMemory0: " + Runtime.getRuntime().freeMemory());
 
-        Runtime.getRuntime().gc();
+        collectAll();
 
         long totalMemory = Runtime.getRuntime().totalMemory();
         long freeMemory = Runtime.getRuntime().freeMemory();
@@ -32,7 +42,7 @@ public class TestRuntime implements Testlet {
 
         array = null;
 
-        Runtime.getRuntime().gc();
+        collectAll();
 
         System.out.println("freeMemory3: " + Runtime.getRuntime().freeMemory());
 
