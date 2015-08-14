589bae27641cae32a6986891d144d61eb5a57d6a
Myk Melez <myk@mozilla.org>
2015-08-13 17:45:07 -0700

    benchmark allocation of arrays of various lengths

---
+++ bench/ArrayBench.java
@@ -0,0 +1,30 @@
+package benchmark;
+
+import com.sun.cldchi.jvm.JVM;
+
+public class ArrayBench {
+    void runBenchmark() {
+        long start, time;
+        int len = 1;
+        int iterations = 1000;
+
+        System.out.println("Time to allocate int arrays of various lengths " + iterations + " times, by length, in ms:");
+
+        for (int i = 0; i < 20; i++) {
+
+            start = JVM.monotonicTimeMillis();
+            for (int j = 0; j < iterations; j++) {
+                int[] array = new int[len];
+            }
+            time = JVM.monotonicTimeMillis() - start;
+            System.out.println(len + ": " + time);
+
+            len = len * 2;
+        }
+    }
+
+    public static void main(String args[]) {
+        ArrayBench bench = new ArrayBench();
+        bench.runBenchmark();
+    }
+}
