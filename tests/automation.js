59a3fc131233bdb89b2cc7f3d51bbed07d009af9
Myk Melez <myk@mozilla.org>
2015-08-20 16:17:44 -0700

    update number of unit tests to run

--- tests/automation.js
+++ tests/automation.js
@@ -180,7 +180,7 @@ function syncFS() {
     });
 }
 
-casper.test.begin("unit tests", 37 + gfxTests.length, function(test) {
+casper.test.begin("unit tests", 39 + gfxTests.length, function(test) {
     casper.start("data:text/plain,start");
 
     casper.page.onLongRunningScript = function(message) {
